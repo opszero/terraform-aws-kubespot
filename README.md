5ec452fb4522c6f4366ce5532edb4de9816bc931
Abhi Yerra <abhi@berkeley.edu>
2017-06-23 15:07:42 -0700

    Include the first image.

---
+++ README.md
@@ -0,0 +1,22 @@
+# auditkube
+
+Secure Images to use with [Kubernetes
+Kops](https://github.com/kubernetes/kops) to help create images which help
+with PCI/HIPAA Compliance.
+
+# Features
+
+ - [X] Encrypted Root Volume
+ - [ ] [OSSEC](https://ossec.github.io/)
+
+# opsZero
+
+This project is brought to you by [opsZero](https://www.opszero.com) we
+provide DevOps and Cloud Infrastructure as a Service for Startups. If you
+need help with your infrastructure reach out.
+
+# License
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
---
+++ ami/ossec.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+
+ossec_version="2.9.0" ;
+ossec_checksum="abd5741dc474cbce5cc116f46a5ef2528c847918" ;
+
+sudo apt-get update -y && sudo apt-get install -y build-essential
+
+cd /tmp/
+wget https://github.com/ossec/ossec-hids/archive/${ossec_version}.tar.gz
+mv ${ossec_version}.tar.gz ossec-hids-${ossec_version}.tar.gz
+checksum=$(sha1sum ossec-hids-${ossec_version}.tar.gz | cut -d" " -f1)
+
+if [ $checksum == $ossec_checksum ]
+then
+    tar xfz ossec-hids-${ossec_version}.tar.gz
+    cd ossec-hids-${ossec_version}
+    cp /home/admin/ossec_preloaded-vars.conf ./etc/preloaded-vars.conf
+    cat ./etc/preloaded-vars.conf
+    sudo bash ./install.sh
+    sudo /var/ossec/bin/ossec-control start
+    # sudo /var/ossec/bin/ossec-control status
+
+    # curl http://169.254.169.254/latest/meta-data/local-ipv4
+else
+    "Wrong checksum. Download again or check if file has been tampered with."
+fi
+
+sudo apt-get purge -y build-essential
+sudo apt-get autoremove -y
