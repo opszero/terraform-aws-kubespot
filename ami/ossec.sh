ec961a2f6fc22ed228771ae74cca75b75e2c6417
Abhi Yerra <abhi@berkeley.edu>
2017-08-22 03:24:31 +0000

    GPG Verify OSSEC Packages

--- ami/ossec.sh
+++ ami/ossec.sh
@@ -4,27 +4,23 @@ ossec_version="2.9.1"
 
 sudo apt-get update -y && sudo apt-get install -y build-essential
 
-cd /tmp/
+pushd /tmp/
 wget https://github.com/ossec/ossec-hids/archive/${ossec_version}.tar.gz
 wget https://github.com/ossec/ossec-hids/releases/download/${ossec_version}/ossec-hids-${ossec_version}.tar.gz.asc
+wget https://ossec.github.io/files/OSSEC-ARCHIVE-KEY.asc
+gpg --import OSSEC-ARCHIVE-KEY.asc
 gpg --verify ossec-hids-${ossec_version}.tar.gz.asc ${ossec_version}.tar.gz
 mv ${ossec_version}.tar.gz ossec-hids-${ossec_version}.tar.gz
-checksum=$(sha1sum ossec-hids-${ossec_version}.tar.gz | cut -d" " -f1)
 
-if [ $checksum == $ossec_checksum ]
-then
-    tar xfz ossec-hids-${ossec_version}.tar.gz
-    cd ossec-hids-${ossec_version}
-    cp /home/admin/ossec_preloaded-vars.conf ./etc/preloaded-vars.conf
-    cat ./etc/preloaded-vars.conf
-    sudo bash ./install.sh
-    sudo /var/ossec/bin/ossec-control start
-    # sudo /var/ossec/bin/ossec-control status
-
-    # curl http://169.254.169.254/latest/meta-data/local-ipv4
-else
-    "Wrong checksum. Download again or check if file has been tampered with."
-fi
+tar xfz ossec-hids-${ossec_version}.tar.gz
+cd ossec-hids-${ossec_version}
+cp /home/admin/ossec_preloaded-vars.conf ./etc/preloaded-vars.conf
+cat ./etc/preloaded-vars.conf
+sudo bash ./install.sh
+sudo /var/ossec/bin/ossec-control start
+# sudo /var/ossec/bin/ossec-control status
 
 sudo apt-get purge -y build-essential
 sudo apt-get autoremove -y
+
+popd
