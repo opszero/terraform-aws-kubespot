02f283242bce87cf2f1f7e51c1c0bd82511d28ea
Abhi Yerra <abhi@berkeley.edu>
2019-06-18 17:41:05 -0700

    Update

--- eks/main.tf
+++ eks/main.tf
@@ -317,22 +317,18 @@ resource "aws_autoscaling_group" "nodes_green" {
 data "aws_caller_identity" "current" {
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster-name
-}
-
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster-name
 }
 
 provider "kubernetes" {
-  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
+  host                   = "${aws_eks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)}"
   token                  = "${data.aws_eks_cluster_auth.cluster.token}"
   load_config_file       = false
 }
 
-resource "kubernetes_config_map" "config-map-aws-auth" {
+resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
