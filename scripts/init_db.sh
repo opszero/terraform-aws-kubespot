fa4153026d954c8ed0f96de3df7c1a8cdc3ac78e
Kyle Barton <kyle.humberto@gmail.com>
2019-07-21 11:57:43 +0200

    Set DATABASE_PASSWORD to be DATABASE_OWNER_PASSWORD
    This is because we don't have access to modify the rails credentials
    without the rails master key

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,17 +7,15 @@ timeout 10 /scripts/db_wait.sh
 
 source /scripts/set_env.sh
 
-echo $rails_env
-
 if rake db:exists DATABASE_USER=deployer
 then
-    DATABASE_USER=$DATABASE_OWNER rake db:migrate
+    DATABASE_USER=$DATABASE_OWNER DATABASE_PASSWORD=$DATABASE_OWNER_PASSWORD rake db:migrate
 else
     # create a database using the deployer account and set the
     # ownership to the service user
     DATABASE_USER=deployer rake db:create
     DATABASE_USER=deployer rake db:alter_owner
     DATABASE_USER=deployer rake db:add_extensions
-    DATABASE_USER=$DATABASE_OWNER rake db:migrate
-    DATABASE_USER=$DATABASE_OWNER rake db:seed
+    DATABASE_USER=$DATABASE_OWNER DATABASE_PASSWORD=$DATABASE_OWNER_PASSWORD rake db:migrate
+    DATABASE_USER=$DATABASE_OWNER DATABASE_PASSWORD=$DATABASE_OWNER_PASSWORD rake db:seed
 fi
