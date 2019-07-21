96c5426f2980be45f5172a68d718cb63f178754a
Kyle Barton <kyle.humberto@gmail.com>
2019-07-21 12:50:47 +0200

    Add check for set_env

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-source ./scripts/set_env.sh
+# Check if set_env exists and source it if it does
+if [ -e "./scripts/set_env.sh"]
+then
+    source ./scripts/set_env.sh
+elif [ -e "/scripts/set_env.sh" ]
+then
+    source /scripts/set_env.sh
+fi
 
 if [ "$CLOUD_PROVIDER" = "gcp" ]
 then
@@ -10,5 +17,11 @@ then
     gcloud auth activate-service-account --key-file=$HOME/gcloud-service-key.json
 elif [ "$CLOUD_PROVIDER" = "aws" ]
 then
-    echo "Ensure that AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_DEFAULT_REGION are set"
+    if [ -z "AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_DEFAULT_REGION" ]
+    then
+        echo "Ensure that AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_DEFAULT_REGION are set"
+        exit 1
+    else
+        echo "AWS configured"
+    fi
 fi
