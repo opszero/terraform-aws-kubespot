9d1a0966f6947caff280367a499a6d545f50029f
Abhi Yerra <abhi@berkeley.edu>
2019-07-24 09:53:24 -0700

    Added rails env

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # Wait until the connection is available or timeout after 10 seconds
 timeout 10 /scripts/db_wait.sh
 
 source /scripts/set_env.sh
+
+echo "Rails Env is ${RAILS_ENV}"
 
 if rake db:exists DATABASE_USER=deployer
 then
