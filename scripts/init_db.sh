bfa72ed5d4b5d892bae682337e62fa854fe5cf44
Abhi Yerra <abhi@berkeley.edu>
2019-07-24 10:33:36 -0700

    remove database user

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -11,13 +11,13 @@ echo "Rails Env is ${RAILS_ENV}"
 
 if rake db:exists DATABASE_USER=deployer
 then
-    DATABASE_USER=$DATABASE_OWNER DATABASE_PASSWORD=$DATABASE_OWNER_PASSWORD rake db:migrate
+    rake db:migrate
 else
     # create a database using the deployer account and set the
     # ownership to the service user
-    DATABASE_USER=deployer rake db:create
-    DATABASE_USER=deployer rake db:alter_owner
-    DATABASE_USER=deployer rake db:add_extensions
-    DATABASE_USER=$DATABASE_OWNER DATABASE_PASSWORD=$DATABASE_OWNER_PASSWORD rake db:migrate
-    DATABASE_USER=$DATABASE_OWNER DATABASE_PASSWORD=$DATABASE_OWNER_PASSWORD rake db:seed
+    rake db:create
+    #DATABASE_USER=deployer rake db:alter_owner
+    #DATABASE_USER=deployer rake db:add_extensions
+    rake db:migrate
+    rake db:seed
 fi
