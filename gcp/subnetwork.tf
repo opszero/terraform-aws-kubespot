c000d1cc72fa8086186e0e272ea9ff01fa6e080d
Abhi Yerra <abhi@berkeley.edu>
2019-07-31 19:43:06 -0700

    Fix fmt

--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -2,20 +2,19 @@ resource "google_compute_subnetwork" "subnet" {
   name    = var.cluster_name
   region  = var.region
   network = google_compute_network.network.self_link
-  ip_cidr_range = " "
 
   # TODO: Convert to variable
-    ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.2.0.0/16"
 
 
   enable_flow_logs = true
 
   # TODO: Enable this.
-#   log_config {
-#     aggregation_interval = "INTERVAL_10_MIN"
-#     flow_sampling        = 0.5
-#     metadata             = "INCLUDE_ALL_METADATA"
-  }
+  #   log_config {
+  #     aggregation_interval = "INTERVAL_10_MIN"
+  #     flow_sampling        = 0.5
+  #     metadata             = "INCLUDE_ALL_METADATA"
+  #  }
 }
 
 resource "google_compute_network" "network" {
