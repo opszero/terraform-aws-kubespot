0480b0d72bef1770df58e6e12b44c4c7e70e4725
Abhi Yerra <abhi@berkeley.edu>
2019-07-31 19:42:35 -0700

    Iprange

--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -2,12 +2,19 @@ resource "google_compute_subnetwork" "subnet" {
   name    = var.cluster_name
   region  = var.region
   network = google_compute_network.network.self_link
+  ip_cidr_range = " "
+
+  # TODO: Convert to variable
+    ip_cidr_range = "10.2.0.0/16"
+
 
   enable_flow_logs = true
-  log_config {
-    aggregation_interval = "INTERVAL_10_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
+
+  # TODO: Enable this.
+#   log_config {
+#     aggregation_interval = "INTERVAL_10_MIN"
+#     flow_sampling        = 0.5
+#     metadata             = "INCLUDE_ALL_METADATA"
   }
 }
 
