af5a0ac28484e958ed51095ef8d41b6ac5696e12
Abhi Yerra <abhi@berkeley.edu>
2019-08-02 09:43:02 -0700

    comment

--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -1,22 +1,22 @@
-resource "google_compute_subnetwork" "subnet" {
-  name    = var.cluster_name
-  network = google_compute_network.network
+# resource "google_compute_subnetwork" "subnet" {
+#   name    = var.cluster_name
+#   network = google_compute_network.network
 
-  # TODO: Convert to variable
-  ip_cidr_range = "10.2.0.0/16"
+#   # TODO: Convert to variable
+#   ip_cidr_range = "10.2.0.0/16"
 
 
-  enable_flow_logs = true
+#   enable_flow_logs = true
 
-  # TODO: Enable this.
-  #   log_config {
-  #     aggregation_interval = "INTERVAL_10_MIN"
-  #     flow_sampling        = 0.5
-  #     metadata             = "INCLUDE_ALL_METADATA"
-  #  }
+#   # TODO: Enable this.
+#   #   log_config {
+#   #     aggregation_interval = "INTERVAL_10_MIN"
+#   #     flow_sampling        = 0.5
+#   #     metadata             = "INCLUDE_ALL_METADATA"
+#  #  }
 
-  depends_on = ["google_compute_network.network"]
-}
+#   depends_on = ["google_compute_network.network"]
+# }
 
 resource "google_compute_network" "network" {
   name                    = var.cluster_name
