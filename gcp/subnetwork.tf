b051c4edb44efbe696f0bd59ae3dc8de1057e8c3
Abhi Yerra <abhi@berkeley.edu>
2019-08-02 12:22:46 -0700

    Update subnet

--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -24,18 +24,18 @@ resource "google_compute_network" "network" {
   auto_create_subnetworks = false
 }
 
-# resource "google_compute_global_address" "private_ip_address" {
-#   name          = var.cluster_name
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = 16
-#   network       = google_compute_network.network.self_link
-# }
+resource "google_compute_global_address" "private_ip_address" {
+  name          = var.cluster_name
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.network.self_link
+}
 
 
-# // Set so we can talk to Services like Cloud SQL.
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   network                 = google_compute_network.network.self_link
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# }
+// Set so we can talk to Services like Cloud SQL.
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
