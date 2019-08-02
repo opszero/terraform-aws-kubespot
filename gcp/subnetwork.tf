b6bc02466dfd28a15dfde36a7654035887240fa3
Abhi Yerra <abhi@berkeley.edu>
2019-08-02 12:11:56 -0700

    Added private ip

--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -23,3 +23,11 @@ resource "google_compute_network" "network" {
   name                    = var.cluster_name
   auto_create_subnetworks = false
 }
+
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.network.self_link
+}
