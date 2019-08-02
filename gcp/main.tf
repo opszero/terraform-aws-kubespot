0df607126ac95a70d6bd4c93b40cf9ea271aeadc
Abhi Yerra <abhi@berkeley.edu>
2019-08-02 09:46:22 -0700

    Enable changes

--- gcp/main.tf
+++ gcp/main.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "cluster" {
   name     = var.cluster_name
   location = var.region
 
-  network = google_compute_network.subnet.self_link
+  network = google_compute_network.network.self_link
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -1,22 +1,25 @@
-# resource "google_compute_subnetwork" "subnet" {
-#   name    = var.cluster_name
-#   network = google_compute_network.network
+resource "google_compute_subnetwork" "subnet" {
+  name   = var.cluster_name
+  region = var.region
 
-#   # TODO: Convert to variable
-#   ip_cidr_range = "10.2.0.0/16"
+  network = google_compute_network.network.self_link
+
+  # TODO: Convert to variable
+  ip_cidr_range = "10.2.0.0/16"
 
 
-#   enable_flow_logs = true
 
-#   # TODO: Enable this.
-#   #   log_config {
-#   #     aggregation_interval = "INTERVAL_10_MIN"
-#   #     flow_sampling        = 0.5
-#   #     metadata             = "INCLUDE_ALL_METADATA"
-#  #  }
+  enable_flow_logs = true
 
-#   depends_on = ["google_compute_network.network"]
-# }
+  # TODO: Enable this.
+  #   log_config {
+  #     aggregation_interval = "INTERVAL_10_MIN"
+  #     flow_sampling        = 0.5
+  #     metadata             = "INCLUDE_ALL_METADATA"
+  #  }
+
+  depends_on = ["google_compute_network.network"]
+}
 
 resource "google_compute_network" "network" {
   name                    = var.cluster_name
