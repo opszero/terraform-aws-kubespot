7e2dec98fe6dbf894c76817ec3c36826d49d68d7
Abhi Yerra <abhi@berkeley.edu>
2019-08-02 15:18:06 -0700

    Fix

--- gcp/subnetwork.tf
+++ gcp/subnetwork.tf
@@ -37,5 +37,5 @@ resource "google_compute_global_address" "private_ip_address" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.network.self_link
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  reserved_peering_ranges = [] # [google_compute_global_address.private_ip_address.name]
 }
