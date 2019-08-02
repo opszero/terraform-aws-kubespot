f67fa5503cef568f89d753e570fd86da827459c0
Abhi Yerra <abhi@berkeley.edu>
2019-08-01 17:12:42 -0700

    added network for cluster

--- gcp/main.tf
+++ gcp/main.tf
@@ -2,6 +2,8 @@ resource "google_container_cluster" "cluster" {
   name     = var.cluster_name
   location = var.region
 
+  network = google_compute_network.subnet.self_link
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
