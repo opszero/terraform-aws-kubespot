4e4edb24db9dbaa65b4d76860d17ff8c682170c2
Abhi Yerra <abhi@berkeley.edu>
2019-08-03 12:43:12 -0700

    Wrote todo list

--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import "github.com/opszero/deploytag/cmd"
 
 func main() {
 	cmd.Execute()
+
+	// Base Flags: --cloud [aws|gcp|azure] --aws-access-key-id --aws-secret-access-key --gcp-auth-file
+	// set-env
+	// database test
+	// database wait
+	// k8s configure
+	// k8s secret --aws-secret-manager
+	//    - https://godoc.org/github.com/joho/godotenv#Read
+	// k8s configmap --aws-secret-manager
+	//    - https://godoc.org/github.com/joho/godotenv#Read
+	// framework rails
+	// framework rails bundle
+	// deploy
+	// docker build
+	// docker deps --base-image=debian
 }
