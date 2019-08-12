0bf6cd07d2c11daa42965d186246a19625712cfa
Abhi Yerra <abhi@berkeley.edu>
2019-08-12 15:55:40 -0700

    Add K8s Cluster

--- azure/main.tf
+++ azure/main.tf
@@ -1,39 +1,39 @@
-resource "azurerm_resource_group" "cluster_group" {
+resource "azurerm_resource_group" "cluster" {
   name     = var.cluster_name
   location = var.region
 }
 
-# resource "azurerm_kubernetes_cluster" "test" {
-#   name                = "acctestaks1"
-#   location            = "${azurerm_resource_group.test.location}"
-#   resource_group_name = "${azurerm_resource_group.test.name}"
-#   dns_prefix          = "acctestagent1"
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = var.cluster_name
+  location            = "${azurerm_resource_group.cluster.location}"
+  resource_group_name = "${azurerm_resource_group.cluster.name}"
+  dns_prefix          = "acctestagent1"
 
-#   agent_pool_profile {
-#     name            = "default"
-#     count           = 1
-#     vm_size         = "Standard_D1_v2"
-#     os_type         = "Linux"
-#     os_disk_size_gb = 30
-#   }
+  agent_pool_profile {
+    name            = "nodes-green"
+    count           = 1
+    vm_size         = "Standard_D1_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
 
-#   agent_pool_profile {
-#     name            = "pool2"
-#     count           = 1
-#     vm_size         = "Standard_D2_v2"
-#     os_type         = "Linux"
-#     os_disk_size_gb = 30
-#   }
+  agent_pool_profile {
+    name            = "nodes-blue"
+    count           = 1
+    vm_size         = "Standard_D2_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
 
-#   service_principal {
-#     client_id     = "00000000-0000-0000-0000-000000000000"
-#     client_secret = "00000000000000000000000000000000"
-#   }
+  # service_principal {
+  #   client_id     = "00000000-0000-0000-0000-000000000000"
+  #   client_secret = "00000000000000000000000000000000"
+  # }
 
-#   tags = {
-#     Environment = "Production"
-#   }
-# }
+  tags = {
+    Environment = var.cluster_name
+  }
+}
 
 # output "client_certificate" {
 #   value = "${azurerm_kubernetes_cluster.test.kube_config.0.client_certificate}"
