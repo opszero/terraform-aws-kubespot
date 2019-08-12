ff19fe89ebfaf6c6bf19ca55676b8e2366a68477
Abhi Yerra <abhi@berkeley.edu>
2019-08-12 15:56:58 -0700

    Rename cluster resource

--- azure/main.tf
+++ azure/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "cluster" {
   location = var.region
 }
 
-resource "azurerm_kubernetes_cluster" "test" {
+resource "azurerm_kubernetes_cluster" "cluster" {
   name                = var.cluster_name
   location            = "${azurerm_resource_group.cluster.location}"
   resource_group_name = "${azurerm_resource_group.cluster.name}"
