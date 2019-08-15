77180ccd97154128077525923514c5fa5c5062e3
Abhi Yerra <abhi@berkeley.edu>
2019-08-15 08:02:34 -0700

    Added helm

---
+++ gcp/helm.tf
@@ -0,0 +1,43 @@
+
+provider "helm" {
+  kubernetes {
+    host                   = "${google_container_cluster.cluster.endpoint}"
+    cluster_ca_certificate = "${base64decode(google_container_cluster.cluster.cluster_ca_certificate)}"
+    load_config_file       = false
+  }
+
+  install_tiller  = true
+  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+}
+
+resource "kubernetes_service_account" "tiller" {
+  automount_service_account_token = true
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${kubernetes_service_account.tiller.metadata.0.name}"
+    api_group = ""
+    namespace = "${kubernetes_service_account.tiller.metadata.0.namespace}"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
