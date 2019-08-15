35fd94c1c49120cefb425fdbdd089e2aa3d9732e
Abhi Yerra <abhi@berkeley.edu>
2019-08-15 09:20:09 -0700

    Ignore helm

--- gcp/main.tf
+++ gcp/main.tf
@@ -45,8 +45,11 @@ resource "google_container_node_pool" "nodes_green" {
     }
 
     oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }
