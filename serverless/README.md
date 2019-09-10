caca314bd4c985d2388a51a7e8c4f8f2814eb152
Abhi Yerra <abhi@berkeley.edu>
2019-09-10 15:03:56 -0700

    A separate serverless environment for AWS Lambda and Fargate

---
+++ serverless/README.md
@@ -0,0 +1,11 @@
+# KubeSeed
+
+Serverless for AWS. 
+
+- AWS Fargate
+- AWS Lambda
+
+## Usage
+
+```
+```
---
+++ serverless/redis.tf
@@ -0,0 +1,36 @@
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "databases"
+  subnet_ids = concat(aws_subnet.private.*.id, aws_subnet.public.*.id)
+}
+
+# resource "aws_elasticache_parameter_group" "default" {
+#   name   = "cache-params"
+#   family = "redis2.8"
+
+#   parameter {
+#     name  = "activerehashing"
+#     value = "yes"
+#   }
+
+#   parameter {
+#     name  = "min-slaves-to-write"
+#     value = "2"
+#   }
+# }
+
+resource "aws_elasticache_cluster" "default" {
+  count      = var.redis_enabled ? 1 : 0
+  cluster_id = var.environment_name
+
+  engine    = "redis"
+  node_type = var.redis_node_type
+
+  num_cache_nodes = var.redis_num_nodes
+  engine_version  = "5.0.4"
+  // TODO: Need to implement the parameter group
+  // parameter_group_name = aws_elasticache_parameter_group.default.id
+  port = 6379
+
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
+  security_group_ids = [aws_security_group.node.id]
+}
