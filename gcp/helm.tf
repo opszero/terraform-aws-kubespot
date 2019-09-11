1529ebf636d18cb439db27382fa34f016b8a0499
Abhi Yerra <abhi@berkeley.edu>
2019-09-11 12:33:53 -0700

    test

--- gcp/helm.tf
+++ gcp/helm.tf
@@ -19,24 +19,24 @@ resource "kubernetes_service_account" "tiller" {
   }
 }
 
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
+# resource "kubernetes_cluster_role_binding" "tiller" {
+#   metadata {
+#     name = "tiller"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    kind      = "ServiceAccount"
-    name      = "${kubernetes_service_account.tiller.metadata.0.name}"
-    api_group = ""
-    namespace = "${kubernetes_service_account.tiller.metadata.0.namespace}"
-  }
-}
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "${kubernetes_service_account.tiller.metadata.0.name}"
+#     api_group = ""
+#     namespace = "${kubernetes_service_account.tiller.metadata.0.namespace}"
+#   }
+# }
 
 resource "helm_release" "ingress" {
   name  = "ingress"
