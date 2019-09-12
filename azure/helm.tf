cbfc70fef36d000315b8509905789d0d0e046d1e
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 10:39:19 -0700

    added the info

--- azure/helm.tf
+++ azure/helm.tf
@@ -1,18 +1,18 @@
-# provider "helm" {
-#   install_tiller  = true
-#   service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-#   namespace       = "kube-system"
+provider "helm" {
+  install_tiller  = true
+  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  namespace       = "kube-system"
 
-#   kubernetes {
-#     host                   = "${azurerm_kubernetes_cluster.cluster.kube_config.0.host}"
-#     username               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.username}"
-#     password               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.password}"
-#     client_certificate     = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
-#     client_key             = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)}"
-#     cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
-#     load_config_file       = false
-#   }
-# }
+  kubernetes {
+    host                   = "${azurerm_kubernetes_cluster.cluster.kube_config.0.host}"
+    username               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.username}"
+    password               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.password}"
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
+    load_config_file       = false
+  }
+}
 
 resource "kubernetes_service_account" "tiller" {
   automount_service_account_token = true
