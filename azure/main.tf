e1bf99792fd23b636e240c3117ae061aaf1ac911
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 11:48:35 -0700

    Fix

--- azure/main.tf
+++ azure/main.tf
@@ -10,17 +10,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   dns_prefix          = "auditkube"
 
   agent_pool_profile {
-    name            = "nodesgreen"
-    count           = 1
-    vm_size         = "Standard_D2_v2"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
-
-    vnet_subnet_id = "${azurerm_subnet.cluster.id}"
-  }
-
-  agent_pool_profile {
-    name            = "nodesblue"
+    name            = "nodes"
     count           = 1
     vm_size         = "Standard_D2_v2"
     os_type         = "Linux"
