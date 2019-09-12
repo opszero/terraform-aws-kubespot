b30e87520abfd289e13a6fb41bb6b995af401776
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 11:18:34 -0700

    Fix

--- azure/helm.tf
+++ azure/helm.tf
@@ -1,7 +1,16 @@
 provider "helm" {
   install_tiller  = false
-//  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  //  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
   //namespace = "kube-system"
+  kubernetes {
+    host                   = "${azurerm_kubernetes_cluster.cluster.kube_config.0.host}"
+    username               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.username}"
+    password               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.password}"
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
+    load_config_file       = false
+  }
 }
 
 
