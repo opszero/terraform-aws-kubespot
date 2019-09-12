2e32ff1878cc28b988f017917294288d5ebe1f66
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 11:45:19 -0700

    Added manual step

--- azure/helm.tf
+++ azure/helm.tf
@@ -12,44 +12,6 @@ provider "helm" {
   }
 }
 
-
-resource "null_resource" "helm_init" {
-  provisioner "local-exec" {
-    command = "helm init --wait --replicas 1 --tiller-namespace kube-system --service-account=${kubernetes_service_account.tiller.metadata.0.name}"
-  }
-
-  depends_on = ["kubernetes_cluster_role_binding.tiller"]
-}
-
-
-
-resource "kubernetes_service_account" "tiller" {
-  automount_service_account_token = true
-
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
-
 # resource "helm_release" "ingress" {
 #   name  = "ingress"
 #   chart = "stable/nginx-ingress"
