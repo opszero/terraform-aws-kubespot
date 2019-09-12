dcf1b12217064d51afcb761adee3ceb806c7f09f
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 10:19:33 -0700

    Add namespace

--- azure/helm.tf
+++ azure/helm.tf
@@ -1,6 +1,7 @@
 provider "helm" {
   install_tiller  = true
   service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  namespace = "kube-system"
 
   kubernetes {
     host                   = "${azurerm_kubernetes_cluster.cluster.kube_config.0.host}"
