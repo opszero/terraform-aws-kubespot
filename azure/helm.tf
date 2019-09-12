548b8e38fae306f42e04a2b074c26b83461c3995
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 10:57:07 -0700

    Fix

--- azure/helm.tf
+++ azure/helm.tf
@@ -1,8 +1,8 @@
 provider "helm" {
     version = "~> 0.10.0"
   install_tiller  = true
-  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-  namespace = "${kubernetes_service_account.tiller.metadata.0.namespace}"
+//  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  namespace = "kube-system"
   tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
 
 
