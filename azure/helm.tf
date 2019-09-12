8f9a74014af1fdf15b75417a38df83b4e2fed087
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 11:34:47 -0700

    Fix

--- azure/helm.tf
+++ azure/helm.tf
@@ -1,7 +1,6 @@
 provider "helm" {
   install_tiller  = false
-  //  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-  //namespace = "kube-system"
+  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
   kubernetes {
     host                   = "${azurerm_kubernetes_cluster.cluster.kube_config.0.host}"
     username               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.username}"
