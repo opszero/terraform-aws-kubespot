f32e8fe97633e653d6954618b4f2ae0ff2265463
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 10:18:13 -0700

    test

--- azure/helm.tf
+++ azure/helm.tf
@@ -1,10 +1,20 @@
 provider "helm" {
   install_tiller  = true
   service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+
+  kubernetes {
+    host                   = "${azurerm_kubernetes_cluster.cluster.kube_config.0.host}"
+    username               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.username}"
+    password               = "${azurerm_kubernetes_cluster.cluster.kube_config.0.password}"
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
+  }
 }
 
 resource "kubernetes_service_account" "tiller" {
   automount_service_account_token = true
+
   metadata {
     name      = "tiller"
     namespace = "kube-system"
