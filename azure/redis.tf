6b21e644f0948f76ead87afb6775f0d2474d7cb7
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 21:39:08 -0700

    More fixes

--- azure/redis.tf
+++ azure/redis.tf
@@ -1,4 +1,3 @@
-// TODO: Make these generic
 resource "azurerm_redis_cache" "default" {
   count = var.redis_enabled ? 1 : 0
 
@@ -6,8 +5,8 @@ resource "azurerm_redis_cache" "default" {
   location            = azurerm_resource_group.cluster.location
   resource_group_name = azurerm_resource_group.cluster.name
   capacity            = var.redis_capacity
-  family              = "P"
-  sku_name            = "Premium"
+  family              = var.redis_family
+  sku_name            = var.redis_sku_name
   enable_non_ssl_port = true
   shard_count         = var.redis_shard_count
 
