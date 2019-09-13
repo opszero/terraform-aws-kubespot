9752f65aedd5abb836307ed93743161f0ce7e296
Abhi Yerra <abhi@berkeley.edu>
2019-09-12 17:00:51 -0700

    Fix the quote

--- azure/registry.tf
+++ azure/registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                     = replace(var.environment_name, "-', "")
+  name                     = replace(var.environment_name, "/-/", "")
   resource_group_name      = "${azurerm_resource_group.cluster.name}"
   location                 = "${azurerm_resource_group.cluster.location}"
   sku                      = "Premium"
