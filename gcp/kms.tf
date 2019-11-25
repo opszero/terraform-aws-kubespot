80e0c93736582d20ce814003d1c64c72783f9680
Abhi Yerra <abhi@opszero.com>
2019-11-25 14:14:19 -0800

    Setup KMS (#63)
    * Fix ingress repo add
    
    * Fix the fmt
    
    * Setup KMS Keyring

---
+++ gcp/kms.tf
@@ -0,0 +1,18 @@
+resource "google_kms_key_ring" "keyring" {
+  name     = var.environment_name
+  location = "global"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+resource "google_kms_crypto_key" "example-key" {
+  name            = var.environment_name
+  key_ring        = google_kms_key_ring.keyring.self_link
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
