406f63c990ef664b927ddc4a6e8cdeafb4f16ec2
Abhi Yerra <abhi@opszero.com>
2019-11-25 14:13:24 -0800

    Fix ingress repo add (#61)

--- helm/ingress.sh
+++ helm/ingress.sh
@@ -5,6 +5,8 @@ set -x
 export KUBECONFIG=$1
 
 helm init
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+
 # https://github.com/helm/charts/tree/master/stable/nginx-ingress
 # https://github.com/kubernetes/ingress-nginx/blob/master/docs/user-guide/nginx-configuration/configmap.md
 
