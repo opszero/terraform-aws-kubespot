8f5da31bcec82c013e599303db4c2b15acfc1b97
Abhi Yerra <abhi@berkeley.edu>
2019-12-09 16:28:40 -0800

    Added string

--- eks/inputs.tf
+++ eks/inputs.tf
@@ -68,18 +68,18 @@ variable "nodes_blue_max_size" {
 
 //the following below are required for setting up the vpn
 variable "foxpass_api_key" {
-  type    = "string"
+  type    = string
   default = ""
 }
 
 variable "foxpass_vpn_psk" {
-  type        = "string"
+  type        = string
   description = "use this for psk generation https://cloud.google.com/vpn/docs/how-to/generating-pre-shared-key"
   default     = ""
 }
 
 variable "logdna_ingestion_key" {
-  type    = "string"
+  type    = string
   default = ""
 }
 
