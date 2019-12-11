4d448a4313071252bb76ebb054e7d6d8e39c42fe
drunkengranite <adamalanahrens@gmail.com>
2019-12-11 15:36:16 -0800

    disabled ipv4 public ip

--- gcp/sql.tf
+++ gcp/sql.tf
@@ -10,7 +10,7 @@ resource "google_sql_database_instance" "default" {
   settings {
     tier = var.sql_instance_class
     ip_configuration {
-      ipv4_enabled    = "true"
+      ipv4_enabled    = "false"
       private_network = google_compute_network.network.self_link
     }
   }
