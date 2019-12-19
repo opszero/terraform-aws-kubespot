8d971074134ab56c04b2ecfb7287f03fbdc7cc1f
Abhi Yerra <abhi@berkeley.edu>
2019-12-18 18:38:22 -0800

    Updated

--- eks/sql.tf
+++ eks/sql.tf
@@ -4,7 +4,7 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_rds_cluster" "default" {
-  count = var.sql_enabled ? 1 : 0
+  count = var.sql_enabled && regex("aurora", var.sql_engine) ? 1 : 0
 
   cluster_identifier = var.environment_name
 
@@ -39,14 +39,14 @@ resource "aws_rds_cluster" "default" {
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
-  count = var.sql_engine_mode == "serverless" ? 0 : var.sql_node_count
+  count = var.sql_engine_mode == "serverless" && regex("aurora", var.sql_engine) ? 0 : var.sql_node_count
 
-  engine             = var.sql_engine
-  identifier         = "${var.environment_name}-${count.index}"
+  engine     = var.sql_engine
+  identifier = "${var.environment_name}-${count.index}"
 
   cluster_identifier = aws_rds_cluster.default.0.id
 
-  instance_class     = var.sql_instance_class
+  instance_class = var.sql_instance_class
 
   db_subnet_group_name = aws_db_subnet_group.default.name
 }
--- helm/ingress.sh
+++ helm/ingress.sh
@@ -4,10 +4,9 @@ set -x
 
 export KUBECONFIG=$1
 
-helm init
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 
 # https://github.com/helm/charts/tree/master/stable/nginx-ingress
 # https://github.com/kubernetes/ingress-nginx/blob/master/docs/user-guide/nginx-configuration/configmap.md
 
-helm upgrade --install ingress-controller stable/nginx-ingress --set controller.config.enable-underscores-in-headers='"true"',controller.config.use-forwarded-headers='"true"'
+helm upgrade --install ingress stable/nginx-ingress -f ./values.yaml
