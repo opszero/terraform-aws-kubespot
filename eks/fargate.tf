bd94a7491ab5d9992f13ccda42b0dac017a8a0f5
Abhi Yerra <abhi@berkeley.edu>
2019-12-20 14:48:34 -0800

    Added fargate

---
+++ eks/fargate.tf
@@ -0,0 +1,10 @@
+resource "aws_eks_fargate_profile" "fargate" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "${var.environment_name}-fargate"
+  pod_execution_role_arn = aws_iam_role.fargate.arn
+  subnet_ids             = aws_subnet.private.*.id
+
+  selector {
+    namespace = "fargate"
+  }
+}
