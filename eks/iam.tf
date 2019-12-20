8ab691e4ceae4d39bf5b3ae7cb0726704f8ad193
Abhi Yerra <abhi@berkeley.edu>
2019-12-20 14:43:09 -0800

    Added fargate

--- eks/iam.tf
+++ eks/iam.tf
@@ -93,3 +93,24 @@ resource "aws_iam_role_policy" "autoscaling" {
 }
 EOF
 }
+
+
+resource "aws_iam_role" "fargate" {
+  name = "${var.environment_name}-fargate"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate.name
+}
--- eks/kubernetes.tf
+++ eks/kubernetes.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
-  host                   = "${aws_eks_cluster.cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)}"
-  token                  = "${data.aws_eks_cluster_auth.cluster.token}"
+  host                   = aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
 
