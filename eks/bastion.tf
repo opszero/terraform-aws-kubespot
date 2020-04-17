5745be254e17f021885567a6e91fd0ca27fbabc8
Abhi Yerra <abhi@berkeley.edu>
2020-04-16 17:49:59 -0700

    Disable bastion

--- eks/bastion.tf
+++ eks/bastion.tf
@@ -1,42 +1,42 @@
-resource "aws_eip" "bastion_eip" {
-  count = var.bastion_enabled ? 1 : 0
+# resource "aws_eip" "bastion_eip" {
+#   count = var.bastion_enabled ? 1 : 0
 
-  instance = aws_instance.bastion.0.id
-  vpc      = true
-}
+#   instance = aws_instance.bastion.0.id
+#   vpc      = true
+# }
 
-resource "aws_instance" "bastion" {
-  count = var.bastion_enabled ? 1 : 0
+# resource "aws_instance" "bastion" {
+#   count = var.bastion_enabled ? 1 : 0
 
-  ami           = data.aws_ami.opszero_eks.id
-  instance_type = "t2.micro"
+#   ami           = data.aws_ami.opszero_eks.id
+#   instance_type = "t2.micro"
 
-  key_name                    = var.ec2_keypair
-  associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public[0].id
-  vpc_security_group_ids      = [aws_security_group.node.id]
+#   key_name                    = var.ec2_keypair
+#   associate_public_ip_address = true
+#   subnet_id                   = aws_subnet.public[0].id
+#   vpc_security_group_ids      = [aws_security_group.node.id]
 
-  tags = {
-    Name = "${var.environment_name}-bastion"
-  }
-  user_data = <<SCRIPT
+#   tags = {
+#     Name = "${var.environment_name}-bastion"
+#   }
+#   user_data = <<SCRIPT
 
-if [[ ${var.logdna_ingestion_key} == ""  ]]
-then
-    echo "Not Installing LogDNA."
-else
-    echo "deb https://repo.logdna.com stable main" | sudo tee /etc/apt/sources.list.d/logdna.list
-    wget -O- https://repo.logdna.com/logdna.gpg | sudo apt-key add -
-    apt-get update
-    apt-get install logdna-agent < "/dev/null" # this line needed for copy/paste
-    logdna-agent -k ${var.logdna_ingestion_key} # this is your unique Ingestion Key
-    # /var/log is monitored/added by default (recursively), optionally add more dirs with:
-    # sudo logdna-agent -d /path/to/log/folders
-    # You can configure the agent to tag your hosts with:
-    # sudo logdna-agent -t mytag,myothertag
-    update-rc.d logdna-agent defaults
-    /etc/init.d/logdna-agent start
-fi
+# if [[ ${var.logdna_ingestion_key} == ""  ]]
+# then
+#     echo "Not Installing LogDNA."
+# else
+#     echo "deb https://repo.logdna.com stable main" | sudo tee /etc/apt/sources.list.d/logdna.list
+#     wget -O- https://repo.logdna.com/logdna.gpg | sudo apt-key add -
+#     apt-get update
+#     apt-get install logdna-agent < "/dev/null" # this line needed for copy/paste
+#     logdna-agent -k ${var.logdna_ingestion_key} # this is your unique Ingestion Key
+#     # /var/log is monitored/added by default (recursively), optionally add more dirs with:
+#     # sudo logdna-agent -d /path/to/log/folders
+#     # You can configure the agent to tag your hosts with:
+#     # sudo logdna-agent -t mytag,myothertag
+#     update-rc.d logdna-agent defaults
+#     /etc/init.d/logdna-agent start
+# fi
 
-SCRIPT
-}
+# SCRIPT
+# }
