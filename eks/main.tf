d67284d74dff17543d1d471fcf02505fc25e07c9
Abhi Yerra <abhi@berkeley.edu>
2020-04-17 11:36:59 -0700

    Fix userdata

--- eks/main.tf
+++ eks/main.tf
@@ -71,23 +71,23 @@ locals {
 #!/bin/bash -xe
 set -o xtrace
 
-sysctl -w net.core.netdev_max_backlog=30000
-sysctl -w net.core.rmem_max=16777216
-sysctl -w net.core.somaxconn=16096
-sysctl -w net.core.wmem_max=16777216
-sysctl -w net.ipv4.ip_local_port_range=1024 65535
-sysctl -w net.ipv4.tcp_fin_timeout=15
-sysctl -w net.ipv4.tcp_max_syn_backlog=20480
-sysctl -w net.ipv4.tcp_max_tw_buckets=400000
-sysctl -w net.ipv4.tcp_no_metrics_save=1
-sysctl -w net.ipv4.tcp_rmem=4096 87380 16777216
-sysctl -w net.ipv4.tcp_syn_retries=2
-sysctl -w net.ipv4.tcp_synack_retries=2
-sysctl -w net.ipv4.tcp_syncookies=1
-sysctl -w net.ipv4.tcp_wmem=4096 65536 16777216
-sysctl -w proc.file-max=2097152
-sysctl -w proc.min_free_kbytes=65536
-sysctl -w vm.min_free_kbytes=65536
+sysctl -w net.core.netdev_max_backlog="30000"
+sysctl -w net.core.rmem_max="16777216"
+sysctl -w net.core.somaxconn="16096"
+sysctl -w net.core.wmem_max="16777216"
+sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+sysctl -w net.ipv4.tcp_fin_timeout="15"
+sysctl -w net.ipv4.tcp_max_syn_backlog="20480"
+sysctl -w net.ipv4.tcp_max_tw_buckets="400000"
+sysctl -w net.ipv4.tcp_no_metrics_save="1"
+sysctl -w net.ipv4.tcp_rmem="4096 87380 16777216"
+sysctl -w net.ipv4.tcp_syn_retries="2"
+sysctl -w net.ipv4.tcp_synack_retries="2"
+sysctl -w net.ipv4.tcp_syncookies="1"
+sysctl -w net.ipv4.tcp_wmem="4096 65536 16777216"
+sysctl -w proc.file-max="2097152"
+sysctl -w proc.min_free_kbytes="65536"
+sysctl -w vm.min_free_kbytes="65536"
 
 /etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority[0].data}' '${var.environment_name}'
 USERDATA
