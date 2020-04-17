4761f55e4fbb4a0cd1a3249f3e168a78891d651b
Abhi Yerra <abhi@berkeley.edu>
2020-04-16 17:41:26 -0700

    Removed the AMI

--- eks/ami.tf
+++ eks/ami.tf
@@ -1,14 +1,3 @@
-data "aws_ami" "opszero_eks" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["opszero-eks-*"]
-  }
-
-  owners = ["self"]
-}
-
 data "aws_ami" "foxpass_vpn" {
 
   most_recent = true
