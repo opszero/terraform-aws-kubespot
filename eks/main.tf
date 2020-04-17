27569f3932f139ee39be8f1100e36acf91013266
Abhi Yerra <abhi@berkeley.edu>
2020-04-16 17:36:13 -0700

    Fix the thingy

--- eks/main.tf
+++ eks/main.tf
@@ -45,81 +45,30 @@ resource "aws_eks_addon" "core" {
   ]
 }
 
-# EKS currently documents this required userdata for EKS worker nodes to
-# properly configure Kubernetes applications on the EC2 instance.
-# We utilize a Terraform local here to simplify Base64 encoding this
-# information into the AutoScaling Launch Configuration.
-# More information: https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/amazon-eks-nodegroup.yaml
-locals {
-  node-userdata = <<USERDATA
-#!/bin/bash -xe
-set -o xtrace
 
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority[0].data}' '${var.environment_name}'
-USERDATA
+resource "aws_eks_node_group" "workers" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.environment_name}-workers"
+  node_role_arn   = aws_iam_role.cluster.arn
+  subnet_ids = flatten([
+    aws_subnet.public.*.id,
+    aws_subnet.private.*.id,
+  ])
 
-}
-
-resource "aws_launch_configuration" "nodes" {
-  associate_public_ip_address = false
-  iam_instance_profile        = aws_iam_instance_profile.node.name
-  image_id                    = var.ami_image == "" ? data.aws_ssm_parameter.eks_ami.value : var.ami_image
-  instance_type               = var.nodes_blue_instance_type
-  name_prefix                 = "${var.environment_name}-nodes-blue"
-  security_groups             = [aws_security_group.node.id]
-  user_data_base64            = base64encode(local.node-userdata)
-  associate_public_ip_address = var.nodes_in_public_subnet
-
-  key_name = var.ec2_keypair
-
-  root_block_device {
-    volume_size = var.nodes_blue_root_device_size
-    encrypted   = true
+  disk_size      = var.nodes_disk_size
+  instance_types = var.nodes_instance_types
+  scaling_config {
+    desired_size = var.nodes_desired_capacity
+    max_size     = var.nodes_max_size
+    min_size     = var.nodes_min_size
   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "nodes" {
-  desired_capacity      = var.nodes_blue_desired_capacity
-  launch_configuration  = aws_launch_configuration.nodes_blue.id
-  max_size              = var.nodes_blue_max_size
-  min_size              = var.nodes_blue_min_size
-  name                  = "${var.environment_name}-nodes-blue"
-  max_instance_lifetime = var.nodes_blue_max_instance_lifetime
-
-  vpc_zone_identifier = length(var.nodes_blue_subnet_ids) == 0 ? (var.nodes_in_public_subnet ? aws_subnet.public.*.id : aws_subnet.private.*.id) : var.nodes_blue_subnet_ids
-
-  enabled_metrics = var.enabled_metrics_asg
-
-  tags = [
-    {
-      key                 = "Name"
-      value               = "${var.environment_name}-nodes-blue"
-      propagate_at_launch = true
-    },
-    {
-      key                 = "kubernetes.io/cluster/${var.environment_name}"
-      value               = "owned"
-      propagate_at_launch = true
-    },
-    {
-      key                 = "k8s.io/cluster-autoscaler/${var.environment_name}"
-      value               = "owned"
-      propagate_at_launch = true
-    },
-    {
-      key                 = "k8s.io/cluster-autoscaler/enabled"
-      value               = "TRUE"
-      propagate_at_launch = true
-    },
-    {
-      key                 = "KubespotEnvironment"
-      value               = var.environment_name
-      propagate_at_launch = true
-    },
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.cluster-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.cluster-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
