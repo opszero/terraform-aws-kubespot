f60ea067ce759245d5433a50803ebb5e26c0b365
Abhi Yerra <abhi@berkeley.edu>
2020-04-16 17:46:29 -0700

    Moved iam to the appropriate places

--- eks/fargate.tf
+++ eks/fargate.tf
@@ -12,3 +12,23 @@ resource "aws_eks_fargate_profile" "fargate" {
     "KubespotEnvironment" = var.environment_name
   }
 }
+
+resource "aws_iam_role" "fargate" {
+  name = "${var.environment_name}-fargate"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate.name
+}
---
+++ eks/workers.tf
@@ -0,0 +1,60 @@
+resource "aws_eks_node_group" "workers" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.environment_name}-workers"
+  node_role_arn   = aws_iam_role.cluster.arn
+  subnet_ids = flatten([
+    aws_subnet.public.*.id,
+    aws_subnet.private.*.id,
+  ])
+
+  disk_size      = var.nodes_disk_size
+  instance_types = var.nodes_instance_types
+  scaling_config {
+    desired_size = var.nodes_desired_capacity
+    max_size     = var.nodes_max_size
+    min_size     = var.nodes_min_size
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_iam_role" "node" {
+  name = "${var.environment_name}-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
