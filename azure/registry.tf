ef0afac0efbc591948885f8e40223a3743662f25
Abhi Yerra <abhi@berkeley.edu>
2020-10-21 09:50:56 -0700

    Fixes

--- azure/registry.tf
+++ azure/registry.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "acr" {
   name                     = replace(var.environment_name, "/-/", "")
-  resource_group_name      = "${azurerm_resource_group.cluster.name}"
-  location                 = "${azurerm_resource_group.cluster.location}"
+  resource_group_name      = azurerm_resource_group.cluster.name
+  location                 = azurerm_resource_group.cluster.location
   sku                      = "Premium"
   admin_enabled            = false
   georeplication_locations = ["East US", "West Europe"]
