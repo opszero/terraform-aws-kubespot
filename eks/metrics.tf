39ca177c0e58f001ae2d569ecf277aa3db881ed9
Abhi Yerra <abhi@berkeley.edu>
2020-11-11 17:25:59 -0500

    addev vpc flow log

---
+++ eks/metrics.tf
@@ -0,0 +1,21 @@
+# resource "aws_cloudwatch_metric_alarm" "cpu_threshold" {
+#   alarm_name                = "${var.environment_name}-cpu-threshold"
+#   comparison_operator       = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods        = "2"
+#   metric_name               = "CPUUtilization"
+#   namespace                 = "AWS/EC2"
+#   period                    = "300"
+#   statistic                 = "Average"
+#   threshold                 = "80"
+#   alarm_description         = "This metric monitors ec2 cpu utilization"
+#   insufficient_data_actions = []
+
+#   dimensions = {
+
+#   }
+#}
+#
+
+resource "aws_cloudwatch_log_group" "group" {
+  name = var.environment_name
+}
