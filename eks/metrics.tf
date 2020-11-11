01521ac8577573a606e63f988bc622391169e2d5
Abhi Yerra <abhi@berkeley.edu>
2020-11-11 17:48:49 -0500

    Added some stuff

--- eks/metrics.tf
+++ eks/metrics.tf
@@ -1,5 +1,9 @@
+resource "aws_cloudwatch_log_group" "vpc" {
+  name = var.environment_name
+}
+
 resource "aws_cloudwatch_metric_alarm" "nodes_green_cpu_threshold" {
-  alarm_name                = "${var.environment_name}-cpu-threshold"
+  alarm_name                = "${var.environment_name}-nodes-green-cpu-threshold"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
   metric_name               = "CPUUtilization"
@@ -15,7 +19,45 @@ resource "aws_cloudwatch_metric_alarm" "nodes_green_cpu_threshold" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "nodes_blue_cpu_threshold" {
+  alarm_name                = "${var.environment_name}-nodes-blue-cpu-threshold"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
 
-resource "aws_cloudwatch_log_group" "vpc" {
-  name = var.environment_name
+  dimensions = {
+    auto_scaling_group_name = aws_autoscaling_group.nodes_blue.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "database_cpu_database" {
+  alarm_name                = "${var.environment_name}-cpu-database"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/RDS"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors RDS cpu utilization"
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "database_disk_database" {
+  alarm_name                = "${var.environment_name}-cpu-database"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "VolumeReadIOPs"
+  namespace                 = "AWS/RDS"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors RDS cpu utilization"
+  insufficient_data_actions = []
 }
