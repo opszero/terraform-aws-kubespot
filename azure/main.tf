208698bf0b81ef7e8111a1531f822b2f848e1ad2
Abhi Yerra <abhi@berkeley.edu>
2020-11-12 00:34:42 -0500

    Fix

--- azure/main.tf
+++ azure/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     os_disk_size_gb = 30
 
     vnet_subnet_id = azurerm_subnet.cluster.id
+    enable_node_public_ip = true # TURN it off
   }
 
   service_principal {
