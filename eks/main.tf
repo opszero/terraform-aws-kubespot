022b742b453d7ab0579e29d795855202da775680
Abhi Yerra <abhi@berkeley.edu>
2020-12-02 17:40:16 -0800

    Fix

--- eks/main.tf
+++ eks/main.tf
@@ -75,27 +75,6 @@ locals {
 #!/bin/bash -xe
 set -o xtrace
 
-# echo "net.core.netdev_max_backlog=30000" >> /etc/sysctl.conf
-# echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
-# echo "net.core.somaxconn=16096" >> /etc/sysctl.conf
-# echo "net.core.wmem_max=16777216" >> /etc/sysctl.conf
-# echo "net.ipv4.ip_local_port_range=1024 65535" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_max_syn_backlog=20480" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_max_tw_buckets=400000" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_no_metrics_save=1" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_rmem=4096 87380 16777216" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_syn_retries=2" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_synack_retries=2" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_syncookies=1" >> /etc/sysctl.conf
-# echo "net.ipv4.tcp_wmem=4096 65536 16777216" >> /etc/sysctl.conf
-# echo "proc.file-max=2097152" >>  /etc/sysctl.conf
-# echo "proc.min_free_kbytes=65536" >>  /etc/sysctl.conf
-# echo "vm.min_free_kbytes=65536" >>  /etc/sysctl.conf
-# sysctl -p /etc/sysctl.conf
-
-${var.instance_userdata}
-
 /etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority[0].data}' '${var.environment_name}'
 USERDATA
 
