37f4c0cda2a07c3f32bccd76aafaa9e3f4c9c0c8
Jana <vjanarthanan6@gmail.com>
2021-05-27 17:15:00 +0200

    Implement EFS
    Closes https://github.com/opszero/kubespot/issues/129
    
    * EFS > IAM role for eks service account and efs helm deployment
    
    
    https://docs.aws.amazon.com/eks/latest/userguide/efs-csi.html
    
    * EFS > refactor code

---
+++ eks/efs.tf
@@ -0,0 +1,22 @@
+resource "helm_release" "aws_efs_csi_driver" {
+  count = var.efs_enabled ? 1 : 0
+  name       = "aws-efs-csi-driver"
+
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+  chart      = "aws-efs-csi-driver"
+  depends_on = [module.iam_assumable_role_admin]
+
+  values = [<<EOF
+image:
+  repository: 602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-efs-csi-driver
+
+serviceAccount:
+  controller:
+    create: true
+    name: efs-csi-controller-sa
+    ## Enable if EKS IAM for SA is used
+    annotations:
+      eks.amazonaws.com/role-arn: "${module.iam_assumable_role_admin[0].this_iam_role_name}"
+EOF
+  ]
+}
