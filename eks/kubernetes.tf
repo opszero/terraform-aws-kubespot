ef2800c92aec2c569cdbaf9466a9f8e1e7b4d263
Jana <vjanarthanan6@gmail.com>
2021-06-10 20:25:29 +0200

    EKS > Add SSO roles for EKS Kubernetes

--- eks/kubernetes.tf
+++ eks/kubernetes.tf
@@ -20,6 +20,22 @@ resource "kubernetes_config_map" "aws_auth" {
   groups:
     - system:bootstrappers
     - system:nodes
+- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AWSReservedSSO_AD-EKS-Admins_b2abd90bad1696ac
+      username: adminuser:{{SessionName}}
+      groups:
+        - default:ad-eks-admins
+- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AWSReservedSSO_AD-EKS-ReadOnly_2c5eb8d559b68cb5
+  username: readonlyuser:{{SessionName}}
+  groups:
+    - default:ad-eks-readonly
+- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AWSReservedSSO_AD-EKS-Developers_ac2b0d744059fcd6
+  username: devuser:{{SessionName}}
+  groups:
+    - default:ad-eks-developers
+- rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AWSReservedSSO_AD-EKS-Monitoring-Admins_ac2b0d744059fcd6
+  username: monitoringadminuser:{{SessionName}}
+  groups:
+    - default:ad-eks-monitoring-admins
 CONFIGMAPAWSAUTH
 
     mapUsers = <<CONFIGMAPAWSUSERS
---
+++ eks/sso-rbac.tf
@@ -0,0 +1,166 @@
+#Role and RoleBinding
+resource "kubernetes_role" "default_eks_admins" {
+  metadata {
+    name = "default:ad-eks-admins"
+    namespace = "default"
+    labels = {
+      sso_role = "default:ad-eks-admins"
+    }
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_role_binding" "default_eks_admins" {
+  metadata {
+    name      = "eks-admins-binding"
+    namespace = "default"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.default_eks_admins.metadata.name
+  }
+  subject {
+    kind      = "Group"
+    name      = kubernetes_role.default_eks_admins.metadata.name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_role" "default_eks_readonly" {
+  metadata {
+    name = "default:ad-eks-readonly"
+    namespace = "default"
+    labels = {
+      sso_role = "default:ad-eks-readonly"
+    }
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_role_binding" "default_eks_readonly" {
+  metadata {
+    name      = "eks-readonly-binding"
+    namespace = "default"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.default_eks_readonly.metadata.name
+  }
+  subject {
+    kind      = "Group"
+    name      = kubernetes_role.default_eks_readonly.metadata.name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_role" "default_eks_developers" {
+  metadata {
+    name = "default:ad-eks-developers"
+    namespace = "default"
+    labels = {
+      sso_role = "default:ad-eks-developers"
+    }
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["services","deployments", "pods", "configmaps", "pods/log"]
+    verbs      = ["get", "list", "watch", "update", "create", "patch"]
+  }
+}
+
+resource "kubernetes_role_binding" "default_eks_developers" {
+  metadata {
+    name      = "eks-dev-binding"
+    namespace = "default"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.default_eks_developers.metadata.name
+  }
+  subject {
+    kind      = "Group"
+    name      = kubernetes_role.default_eks_developers.metadata.name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_role" "default_eks_monitoring_admins" {
+  metadata {
+    name = "default:ad-eks-monitoringadmins"
+    namespace = "monitoring"
+    labels = {
+      sso_role = "default:ad-eks-monitoringadmins"
+    }
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_role_binding" "default_eks_monitoring_admins" {
+  metadata {
+    name      = "eks-monitoringadmins"
+    namespace = "monitoring"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.default_eks_monitoring_admins.metadata.name
+  }
+  subject {
+    kind      = "Group"
+    name      = kubernetes_role.default_eks_monitoring_admins.metadata.name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+#ClusterRoleBinding
+resource "kubernetes_cluster_role_binding" "eks_admins_binding" {
+  metadata {
+    name = "clusterrole-eks-admins-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "Group"
+    name      = "default:ad-eks-admins"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "eks_readonly_binding" {
+  metadata {
+    name = "clusterrole-eks-readonly-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "system:aggregate-to-view"
+  }
+  subject {
+    kind      = "Group"
+    name      = "default:ad-eks-readonly"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
