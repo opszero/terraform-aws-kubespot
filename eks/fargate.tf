906aa608cd28b7991119e14504daa809d8da2c3e
janajs <86019052+janajs@users.noreply.github.com>
2021-07-08 20:48:25 +0200

    Fargate > Name selector fix (#145)
    Make fargate as optional.
    
    Co-authored-by: Janarthanan <janarthanan6@gmail.com>

--- eks/fargate.tf
+++ eks/fargate.tf
@@ -1,11 +1,12 @@
 resource "aws_eks_fargate_profile" "fargate" {
+  count = var.fargate_enabled ? 1 : 0
   cluster_name           = aws_eks_cluster.cluster.name
   fargate_profile_name   = "${var.environment_name}-fargate"
   pod_execution_role_arn = aws_iam_role.fargate.arn
   subnet_ids             = aws_subnet.private.*.id
 
   selector {
-    namespace = "fargate"
+    namespace = var.fargate_namespace_selector_name
   }
   tags = {
     "KubespotEnvironment" = var.environment_name
