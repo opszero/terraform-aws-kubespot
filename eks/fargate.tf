b982ce7f5e6dc0cddfbc595d8ec604d590c97d6c
Abhi Yerra <abhi@berkeley.edu>
2021-07-14 15:23:07 -0700

    terraform fmt

--- eks/fargate.tf
+++ eks/fargate.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_fargate_profile" "fargate" {
-  count = var.fargate_enabled ? 1 : 0
+  count                  = var.fargate_enabled ? 1 : 0
   cluster_name           = aws_eks_cluster.cluster.name
   fargate_profile_name   = "${var.environment_name}-serverless"
   pod_execution_role_arn = aws_iam_role.fargate.arn
