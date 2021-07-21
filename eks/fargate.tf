951484a3b6add7552b1a595204c481979735f55f
Abhi Yerra <abhi@berkeley.edu>
2021-07-21 12:22:04 -0700

    running terraform fmt

--- eks/fargate.tf
+++ eks/fargate.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_fargate_profile" "fargate" {
-  for_each = var.fargate_selector
+  for_each               = var.fargate_selector
   cluster_name           = aws_eks_cluster.cluster.name
   fargate_profile_name   = "${var.environment_name}-${each.key}"
   pod_execution_role_arn = lookup(each.value, "role_arn", aws_iam_role.fargate.arn)
