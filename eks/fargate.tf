269e560b799208d532347ff7d30330c16eca74ed
janajs <86019052+janajs@users.noreply.github.com>
2021-07-21 20:47:56 +0200

    Fargate > Loop namesapce (#154)

--- eks/fargate.tf
+++ eks/fargate.tf
@@ -1,12 +1,12 @@
 resource "aws_eks_fargate_profile" "fargate" {
-  count                  = var.fargate_enabled ? 1 : 0
+  for_each = var.fargate_selector
   cluster_name           = aws_eks_cluster.cluster.name
-  fargate_profile_name   = "${var.environment_name}-serverless"
-  pod_execution_role_arn = aws_iam_role.fargate.arn
+  fargate_profile_name   = "${var.environment_name}-${each.key}"
+  pod_execution_role_arn = lookup(each.value, "role_arn", aws_iam_role.fargate.arn)
   subnet_ids             = aws_subnet.private.*.id
 
   selector {
-    namespace = var.fargate_namespace_selector_name
+    namespace = each.key
   }
   tags = {
     "KubespotEnvironment" = var.environment_name
