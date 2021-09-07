9ab4ced2982ff9ca38c773183b47473b8b0d457b
Abhi Yerra <abhi@berkeley.edu>
2021-09-07 10:16:16 -0700

    Fix

--- eks/metrics-server.tf
+++ eks/metrics-server.tf
@@ -2,7 +2,7 @@ resource "helm_release" "metrics-server" {
   count      = var.metrics_server_enabled ? 1 : 0
   name       = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "bitnami/metrics-server"
+  chart      = "metrics-server"
   namespace  = "kube-system"
   wait       = false
 }
