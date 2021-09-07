c552a790ab031efaac02d639015ee2a315706453
Abhi Yerra <abhi@berkeley.edu>
2021-09-07 10:14:50 -0700

    Fix

--- eks/metrics-server.tf
+++ eks/metrics-server.tf
@@ -3,7 +3,6 @@ resource "helm_release" "metrics-server" {
   name       = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "bitnami/metrics-server"
-  version    = var.metrics_server_version
   namespace  = "kube-system"
   wait       = false
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -378,7 +378,3 @@ variable "fargate_selector" {
 variable "metrics_server_enabled" {
   default = true
 }
-
-variable "metrics_server_version" {
-  default = "5.9.3"
-}
