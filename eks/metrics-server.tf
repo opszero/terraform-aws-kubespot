f5ca377c8ea41bdc2e3b97e87514fa35c356596b
Abhi Yerra <abhi@berkeley.edu>
2021-09-07 09:44:02 -0700

    Fix

--- eks/metrics-server.tf
+++ eks/metrics-server.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "metrics-server" {
   count      = var.metrics_server_enabled ? 1 : 0
-  name       = metrics-server
+  name       = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "bitnami/metrics-server"
   version    = var.metrics_server_version
