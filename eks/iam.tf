4d8e2ab9940d2a4496557c669f7c7b786b25f732
Abhi Yerra <abhi@berkeley.edu>
2021-09-10 11:41:22 -0700

    Node mount

--- eks/iam.tf
+++ eks/iam.tf
@@ -229,6 +229,11 @@ module "iam_assumable_role_admin" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "node-EFS" {
+  policy_arn = aws_iam_policy.arn
+  role       = aws_iam_role.node.name
+}
+
 resource "aws_iam_policy" "efs_policy" {
   name        = "${var.environment_name}-efs-policy"
   description = "EKS cluster policy for EFS"
