d9afa09050aa012ec6d038f4c44251cbea27aa89
Abhi Yerra <abhi@berkeley.edu>
2021-10-19 15:47:48 -0700

    Added node group limit

--- eks/node_groups.tf
+++ eks/node_groups.tf
@@ -4,16 +4,20 @@ resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.environment_name}-${each.key}"
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids = flatten([
-    aws_subnet.private.*.id,
-  ])
 
-  disk_size      = lookup(each.value, "node_disk_size", 20)
+  subnet_ids = length(lookup(each.value "subnet_ids", [])) == 0 ? (var.nodes_in_public_subnet ? aws_subnet.public.*.id : aws_subnet.private.*.id) : lookup(each.value "subnet_ids", [])
+
   instance_types = lookup(each.value, "instance_types", ["t2.micro"])
+  disk_size      = lookup(each.value, "node_disk_size", 20)
+
   scaling_config {
     desired_size = lookup(each.value, "node_desired_capacity", 1)
     max_size     = lookup(each.value, "nodes_max_size", 1)
     min_size     = lookup(each.value, "nodes_min_size", 1)
+  }
+
+  update_config {
+    max_unavailable_percentage = 50
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
