c8c1e6bb0bb5a01f3d43a955e6818d2b0f2d6f9e
jana-work <jana-work@JanarthanansMBP.home>
2021-12-10 20:58:23 +0100

    Karpenter > Use default terraform

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -1,52 +1,85 @@
-resource "aws_cloudformation_stack" "karpenter" {
-  count = var.karpenter_enabled ? 1 : 0
-  name  = "${var.environment_name}-karpenter"
-
-  parameters = {
-    ClusterName = aws_eks_cluster.cluster.name
-  }
-
-  template_body = file("${path.module}/karpenter.yaml")
-
+data "aws_iam_policy" "ssm_managed_instance" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-module "iam_assumable_role_karpenter" {
-  source           = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version          = "3.6.0"
-  create_role      = true
-  role_name        = "${var.environment_name}-${var.karpenter_name}"
-  provider_url     = replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")
-  role_policy_arns = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/KarpenterNodeRole-${aws_eks_cluster.cluster.name}"]
-  # namespace and service account name
-  oidc_fully_qualified_subjects = [
-    "system:serviceaccount:kube-system:${var.karpenter_name}",
-    "system:node:{{EC2PrivateDNSName}}",
-    "system:nodes",
-    "system:bootstrappers"
-  ]
-  tags = {
-    "KubespotEnvironment" = var.environment_name
-  }
-  depends_on = [
-    aws_cloudformation_stack.karpenter
-  ]
+resource "aws_iam_role_policy_attachment" "karpenter_ssm_policy" {
+  count = var.karpenter_enabled ? 1 : 0  
+  role       = aws_iam_role.node.name
+  policy_arn = data.aws_iam_policy.ssm_managed_instance.arn
+}
+
+resource "aws_iam_instance_profile" "karpenter" {
+  count = var.karpenter_enabled ? 1 : 0  
+  name = "KarpenterNodeInstanceProfile-${aws_eks_cluster.cluster.name}"
+  role = aws_iam_role.node.name
+}
+
+module "iam_assumable_role_karpenter" {  
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "4.7.0"
+  create_role                   = true
+  role_name                     = "karpenter-controller-${aws_eks_cluster.cluster.name}"
+  provider_url                  = replace(aws_iam_openid_connect_provider.cluster.url, "https://", "") 
+  oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
+}
+
+resource "aws_iam_role_policy" "karpenter_contoller" {
+  name = "karpenter-policy-${aws_eks_cluster.cluster.name}"
+  role = module.iam_assumable_role_karpenter.iam_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "helm_release" "karpenter" {
-  count            = var.karpenter_enabled ? 1 : 0
-  name             = var.karpenter_name
-  repository       = "https://charts.karpenter.sh"
-  chart            = "karpenter/karpenter"
-  version          = var.karpenter_version
-  create_namespace = true
+  count = var.karpenter_enabled ? 1 : 0  
   namespace        = "karpenter"
-  wait             = true
-  values = [<<EOF
-controller.clusterName = ${aws_eks_cluster.cluster.name}
-controller.clusterEndpoint = ${aws_eks_cluster.cluster.endpoint}
-EOF
-  ]
-  depends_on = [
-    aws_cloudformation_stack.karpenter
-  ]
+  create_namespace = true
+
+  name       = var.karpenter_name
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = var.karpenter_version
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_assumable_role_karpenter.iam_role_arn
+  }
+
+  set {
+    name  = "controller.clusterName"
+    value = aws_eks_cluster.cluster.name
+  }
+
+  set {
+    name  = "controller.clusterEndpoint"
+    value = aws_eks_cluster.cluster.endpoint
+  }
+}
+
+resource "aws_iam_service_linked_role" "spot" {
+  aws_service_name = "spot.amazonaws.com"
 }
