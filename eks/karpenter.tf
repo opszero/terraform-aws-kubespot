70e4a4082292212ddf1a321e4b402c26c957c4dc
janajs <86019052+janajs@users.noreply.github.com>
2021-12-13 23:57:09 +0100

    Add karpenter (#173)
    * Add karpenter
    
    * Karpenter > Use default terraform
    
    * Update eks/karpenter.tf
    
    * Update eks/karpenter.tf
    
    Co-authored-by: jana-work <jana-work@JanarthanansMBP.home>
    Co-authored-by: Abhi Yerra <abhi@opszero.com>

---
+++ eks/karpenter.tf
@@ -0,0 +1,85 @@
+data "aws_iam_policy" "ssm_managed_instance" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_ssm_policy" {
+  count = var.karpenter_enabled ? 1 : 0  
+  role       = aws_iam_role.node.name
+  policy_arn = data.aws_iam_policy.ssm_managed_instance.arn
+}
+
+resource "aws_iam_instance_profile" "karpenter" {
+  count = var.karpenter_enabled ? 1 : 0  
+  name = "KarpenterNodeInstanceProfile-${aws_eks_cluster.cluster.name}"
+  role = aws_iam_role.node.name
+}
+
+module "iam_assumable_role_karpenter" {  
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "4.7.0"
+  create_role                   = true
+  role_name                     = "karpenter-controller-${var.environment_name}"
+  provider_url                  = replace(aws_iam_openid_connect_provider.cluster.url, "https://", "") 
+  oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
+}
+
+resource "aws_iam_role_policy" "karpenter_contoller" {
+  name = "karpenter-policy-${var.environment_name}"
+  role = module.iam_assumable_role_karpenter.iam_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "helm_release" "karpenter" {
+  count = var.karpenter_enabled ? 1 : 0  
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = var.karpenter_name
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = var.karpenter_version
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_assumable_role_karpenter.iam_role_arn
+  }
+
+  set {
+    name  = "controller.clusterName"
+    value = aws_eks_cluster.cluster.name
+  }
+
+  set {
+    name  = "controller.clusterEndpoint"
+    value = aws_eks_cluster.cluster.endpoint
+  }
+}
+
+resource "aws_iam_service_linked_role" "spot" {
+  aws_service_name = "spot.amazonaws.com"
+}
