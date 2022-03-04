d9afb01686ed5c4669aaf0afe9ea303a2bcf1b97
Abhi Yerra <abhi@berkeley.edu>
2022-03-04 15:00:38 -0800

    Added keda

--- helm/keda.tf
+++ helm/keda.tf
@@ -1,8 +1,9 @@
 resource "helm_release" "keda" {
   count = var.keda_enabled ? 1 : 0
 
-  name       = "nginx"
-  repository = "https://kedacore.github.io/charts"
-  chart      = "keda"
-  namespace  = "keda"
+  name             = "nginx"
+  repository       = "https://kedacore.github.io/charts"
+  chart            = "keda"
+  namespace        = "keda"
+  create_namespace = true
 }
