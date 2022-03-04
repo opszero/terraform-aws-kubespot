c3277bf24f46520522e3ed9befffabc6a4b61121
Abhi Yerra <abhi@berkeley.edu>
2022-03-04 14:06:29 -0800

    Disable

--- helm/nginx.tf
+++ helm/nginx.tf
@@ -5,9 +5,9 @@ resource "helm_release" "nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx/ingress-nginx"
 
-  values = [
-    "${file("./nginx.yml")}"
-  ]
+  # values = [
+  #   "${file("./nginx.yml")}"
+  # ]
 
   set {
     name  = "controller.replicaCount"
