66936a1fd485640a6b553dec1c53be250a5cf794
Abhi Yerra <abhi@berkeley.edu>
2022-03-08 15:09:30 -0800

    kms keyring

--- gcp/kms.tf
+++ gcp/kms.tf
@@ -10,7 +10,7 @@ resource "google_kms_key_ring" "keyring" {
 
 resource "google_kms_crypto_key" "key" {
   name     = var.environment_name
-  key_ring = google_kms_key_ring.keyring.self_link
+  key_ring = google_kms_key_ring.keyring.id
 
   lifecycle {
     prevent_destroy = true
