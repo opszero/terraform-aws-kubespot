4d68571c92cb73b4bdce0f380c86cb3cc2435776
Abhi Yerra <abhi@berkeley.edu>
2022-03-10 15:13:50 -0800

    Added gcp

--- gcp/main.tf
+++ gcp/main.tf
@@ -14,7 +14,7 @@ resource "google_container_cluster" "cluster" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
+  remove_default_node_pool = var.cluster_enable_autopilot ? false : true
   initial_node_count       = 1
 
   enable_autopilot = var.cluster_enable_autopilot
