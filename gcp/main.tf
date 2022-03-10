55d61d5d7a3bca491d6d6f1460d052d7c9ca4587
Abhi Yerra <abhi@berkeley.edu>
2022-03-10 15:15:20 -0800

    Autopilot by default

--- gcp/main.tf
+++ gcp/main.tf
@@ -11,13 +11,7 @@ resource "google_container_cluster" "cluster" {
 
   min_master_version = var.cluster_version
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = var.cluster_enable_autopilot ? false : true
-  initial_node_count       = 1
-
-  enable_autopilot = var.cluster_enable_autopilot
+  enable_autopilot = true
 
   master_auth {
     client_certificate_config {
@@ -26,37 +20,37 @@ resource "google_container_cluster" "cluster" {
   }
 }
 
-resource "google_container_node_pool" "nodes" {
-  count = var.cluster_enable_autopilot ? 0 : 1
+# resource "google_container_node_pool" "nodes" {
+#   count = var.cluster_enable_autopilot ? 0 : 1
 
-  name       = "nodes"
-  location   = var.region
-  cluster    = google_container_cluster.cluster.name
-  node_count = 1
+#   name       = "nodes"
+#   location   = var.region
+#   cluster    = google_container_cluster.cluster.name
+#   node_count = 1
 
-  autoscaling {
-    min_node_count = var.nodes_min_size
-    max_node_count = var.nodes_max_size
-  }
+#   autoscaling {
+#     min_node_count = var.nodes_min_size
+#     max_node_count = var.nodes_max_size
+#   }
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+#   management {
+#     auto_repair  = true
+#     auto_upgrade = true
+#   }
 
-  node_config {
-    machine_type = var.nodes_instance_type
+#   node_config {
+#     machine_type = var.nodes_instance_type
 
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-}
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/compute",
+#       "https://www.googleapis.com/auth/devstorage.read_only",
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#       "https://www.googleapis.com/auth/cloud-platform",
+#     ]
+#   }
+# }
