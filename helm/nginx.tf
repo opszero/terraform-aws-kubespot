ed2cd811ed3f3c45e41e37f175d78cd01a57b034
Abhi Yerra <abhi@berkeley.edu>
2022-03-23 14:44:35 -0700

    Added nginx

--- helm/nginx.tf
+++ helm/nginx.tf
@@ -1,9 +1,11 @@
 resource "helm_release" "nginx" {
   count = var.nginx_enabled ? 1 : 0
 
-  name       = "nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
+  name             = "nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "nginx"
+  namespace        = "nginx"
+  create_namespace = true
 
   values = [
     "${file("${path.module}/nginx.yml")}"
