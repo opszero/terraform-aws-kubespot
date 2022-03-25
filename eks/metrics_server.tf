2ca47d91178da807ca6b81f02d65ffd7c90974f1
Abhi Yerra <abhi@berkeley.edu>
2022-03-25 14:53:25 -0700

    Added

--- eks/metrics_server.tf
+++ eks/metrics_server.tf
@@ -1,10 +1,11 @@
 resource "helm_release" "metrics-server" {
-  name       = "metrics-server"
-  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-  chart      = "metrics-server"
-  namespace  = "metrics-server"
-  version    = var.metrics_server_version
-  wait       = false
+  name             = "metrics-server"
+  repository       = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart            = "metrics-server"
+  namespace        = "metrics-server"
+  create_namespace = true
+  version          = var.metrics_server_version
+  wait             = false
   values = [<<EOF
 apiService:
   create: true
