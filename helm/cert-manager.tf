83bf603242187fc09941f097800ff6dbaeb7ad02
Abhi Yerra <abhi@berkeley.edu>
2022-03-25 12:08:02 -0700

    Added cert-manager

---
+++ helm/cert-manager.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "cert-manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+}
