45fc154ba9aa54bd124eaca9675770ae18cf6d2c
Abhi Yerra <abhi@berkeley.edu>
2022-03-25 14:15:56 -0700

    Remove karpenter version

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -63,7 +63,6 @@ resource "helm_release" "karpenter" {
   name       = "karpenter"
   repository = "https://charts.karpenter.sh"
   chart      = "karpenter"
-  version    = var.karpenter_version
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
