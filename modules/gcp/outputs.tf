6b73682849d3ba72524470c94235b07464b678df
Abhi Yerra <abhi@berkeley.edu>
2022-04-15 16:23:58 -0700

    Removed gcp

--- modules/gcp/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "sql_database" {
-  value = length(google_sql_database_instance.default) > 0 ? google_sql_database_instance.default[0].name : ""
-}
-
-output "private_vpc_network" {
-  value = google_compute_network.network.self_link
-}
