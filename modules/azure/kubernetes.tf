3926083c9bfe41c743486f438a13e8cb52b06eb3
Abhi Yerra <abhi@berkeley.edu>
2022-04-15 16:34:20 -0700

    Removed azure

--- modules/azure/kubernetes.tf
+++
@@ -1,9 +0,0 @@
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.cluster.kube_config.0.host
-  username               = azurerm_kubernetes_cluster.cluster.kube_config.0.username
-  password               = azurerm_kubernetes_cluster.cluster.kube_config.0.password
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
-  load_config_file       = false
-}
