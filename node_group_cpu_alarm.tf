637f09c05d20b525c679cfeefa23991097678c3d
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-04-28 11:21:00 +0630

    Update node_group_cpu_alarm.tf

--- node_group_cpu_alarm.tf
+++ node_group_cpu_alarm.tf
@@ -2,7 +2,6 @@ resource "aws_cloudwatch_metric_alarm" "node_group_cpu_threshold" {
   # One Alarm Per One Node Group
   for_each = aws_eks_node_group.node_group
 
-
   alarm_name                = "${var.environment_name}-${each.value.node_group_name}"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
