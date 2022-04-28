d93356828e98c7c65b9313d41979176fff0f2b8c
Abhi Yerra <abhi@berkeley.edu>
2022-04-28 09:23:13 -0700

    Move bastion metric

--- metrics.tf
+++ metrics.tf
@@ -5,29 +5,6 @@ resource "aws_cloudwatch_log_group" "vpc" {
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "bastion_cpu_threshold" {
-  count = var.bastion_enabled ? 1 : 0
-
-  alarm_name                = "${var.environment_name}-bastion-cpu-threshold"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  period                    = "300"
-  statistic                 = "Average"
-  threshold                 = "80"
-  alarm_description         = "This metric monitors ec2 cpu utilization"
-  insufficient_data_actions = []
-
-  dimensions = {
-    InstanceId = aws_instance.bastion[0].id
-  }
-
-  tags = {
-    "KubespotEnvironment" = var.environment_name
-  }
-}
-
 resource "aws_cloudwatch_metric_alarm" "nodes_green_cpu_threshold" {
   alarm_name                = "${var.environment_name}-nodes-green-cpu-threshold"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
