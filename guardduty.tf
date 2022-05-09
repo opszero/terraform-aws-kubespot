cc1de0f5dae43752b1c1222458dd6f8cbb57be79
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-05-09 12:42:23 +0630

    Create guardduty.tf

---
+++ guardduty.tf
@@ -0,0 +1,12 @@
+resource "aws_guardduty_detector" "detector" {
+  enable = true 
+
+  datasources {
+    s3_logs {
+      enable = true
+    }
+    eks_logs {
+      enable = var.eks_guardduty_enabled 
+    }
+  }
+}
