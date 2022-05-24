68e5b970b54e11dc4b9c8c9e1413d938a578f4ca
Abhi Yerra <abhi@opszero.com>
2022-05-23 20:00:42 -0700

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -3,67 +3,21 @@ resource "aws_elasticache_subnet_group" "default" {
   subnet_ids = concat(aws_subnet.private.*.id, aws_subnet.public.*.id)
 }
 
-# resource "aws_elasticache_parameter_group" "default" {
-#   name   = "cache-params"
-#   family = "redis2.8"
+resource "aws_elasticache_cluster" "default" {
+  count      = var.redis_enabled ? 1 : 0
+  cluster_id = var.environment_name
 
-#   parameter {
-#     name  = "activerehashing"
-#     value = "yes"
-#   }
+  engine    = "redis"
+  node_type = var.redis_node_type
 
-#   parameter {
-#     name  = "min-slaves-to-write"
-#     value = "2"
-#   }
-# }
+  num_cache_nodes = 1
+  engine_version  = var.redis_engine_version
+  port = 6379
 
-resource "aws_elasticache_replication_group" "default" {
-  count = var.redis_enabled ? 1 : 0
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
+  security_group_ids = [aws_security_group.node.id]
 
-  replication_group_id       = var.environment_name
-  description                = "redis cluster with autoscaling"
-  node_type                  = "cache.r5.large"
-  port                       = 6379
-  parameter_group_name       = "default.redis6.x.cluster.on"
-  automatic_failover_enabled = true
-  engine                     = "redis"
-  subnet_group_name          = aws_elasticache_subnet_group.default.name
-  security_group_ids         = [aws_security_group.node.id]
-  multi_az_enabled           = true
-
-
-  num_node_groups         = 1
-  replicas_per_node_group = 1
-  tags                    = local.tags
-}
-
-resource "aws_appautoscaling_target" "redis" {
-  count = var.redis_enabled ? 1 : 0
-
-  service_namespace  = "elasticache"
-  scalable_dimension = "elasticache:replication-group:Replicas"
-  resource_id        = "replication-group/${aws_elasticache_replication_group.default[count.index].id}"
-  min_capacity       = 1
-  max_capacity       = 5
-}
-
-resource "aws_appautoscaling_policy" "redis" {
-  count = var.redis_enabled ? 1 : 0
-
-  name               = "cpu-auto-scaling"
-  service_namespace  = aws_appautoscaling_target.redis[count.index].service_namespace
-  scalable_dimension = aws_appautoscaling_target.redis[count.index].scalable_dimension
-  resource_id        = aws_appautoscaling_target.redis[count.index].resource_id
-  policy_type        = "TargetTrackingScaling"
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ElastiCacheReplicaEngineCPUUtilization"
-    }
-
-    target_value       = 70
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
+  tags = {
+    "KubespotEnvironment" = var.environment_name
   }
 }
