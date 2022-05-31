5eac66dc0535d0944b45f71b73a698dcf2f6ac1f
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-06-01 00:15:15 +0630

    Issue 216 - update version to alb helm chart (#220)
    * update chart version
    
    * update chart version
    
    * update version to alb helm chart

--- variables.tf
+++ variables.tf
@@ -535,3 +535,8 @@ variable "tags" {
   #}  
 }
 
+variable "alb_controller_version" {
+  type        = string
+  description = "The chart version of the ALB controller helm chart"
+  default     = "1.4.2"
+}
