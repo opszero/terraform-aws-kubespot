32737f0d62760872274ee4c3c885d76b071cd274
Abhi Yerra <abhi@berkeley.edu>
2022-06-01 09:12:33 -0700

    Upgrade version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.7.0"
+      version = "~> 4.16.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
