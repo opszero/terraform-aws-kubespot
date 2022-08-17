27a64e3296c39ff90f3fbc117ad39ba955015d93
Abhi Yerra <abhi@berkeley.edu>
2022-08-17 13:15:06 -0700

    Fix node

--- variables.tf
+++ variables.tf
@@ -454,7 +454,7 @@ variable "karpenter_name" {
 }
 
 variable "karpenter_version" {
-  default     = "v0.13.2"
+  default     = "v0.15.0"
   description = "The version of the karpenter helm chart"
 }
 
