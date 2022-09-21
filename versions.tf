9c69a00d850abdb0b755531f270418edc2f24550
Abhi Yerra <abhi@berkeley.edu>
2022-09-20 18:06:00 -0700

    Updated version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16.0"
+      version = "~> 4.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
