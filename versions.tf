3891a2916161bb08a8f42582ad8c5becd065a5c1
Abhi Yerra <abhi@berkeley.edu>
2023-01-19 10:34:05 -0800

    Fix region

--- versions.tf
+++ versions.tf
@@ -3,8 +3,9 @@ terraform {
   required_version = ">= 0.15"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.50.0"
+      source                = "hashicorp/aws"
+      version               = "~> 4.50.0"
+      configuration_aliases = [aws.default]
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
