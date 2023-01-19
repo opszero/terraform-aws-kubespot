c73253c3de71cc2db5f08a38e7c209901204d8ca
Abhi Yerra <abhi@berkeley.edu>
2023-01-19 10:20:44 -0800

    Added

--- karpenter.tf
+++ karpenter.tf
@@ -63,10 +63,12 @@ resource "helm_release" "karpenter" {
   namespace        = "karpenter"
   create_namespace = true
 
-  name       = "karpenter"
-  repository = "https://charts.karpenter.sh"
-  chart      = "karpenter"
-  version    = var.karpenter_version
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = var.karpenter_version
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
