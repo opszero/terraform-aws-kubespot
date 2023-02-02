faed6b3baf744e5c7b7e147438c7fb940c8e507f
Abhi Yerra <abhi@berkeley.edu>
2023-02-01 20:43:20 -0800

    Clean up

--- vpc.tf
+++ vpc.tf
@@ -105,13 +105,3 @@ resource "aws_security_group_rule" "private_subnet" {
   cidr_blocks       = var.cidr_block_private_subnet
   type              = "ingress"
 }
-
-resource "aws_security_group_rule" "node-ingress-karpenter" {
-  description              = "Cluster API to Node group for Karpenter webhook"
-  from_port                = 8443
-  to_port                  = 8443
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.node.id
-  source_security_group_id = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
-  type                     = "ingress"
-}
