5ef21b1c458eb55cad8fa407384444683fea57ed
Abhi Yerra <abhi@berkeley.edu>
2023-02-01 20:17:33 -0800

    range fix

--- karpenter.tf
+++ karpenter.tf
@@ -144,7 +144,7 @@ resource "helm_release" "karpenter" {
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.karpenter.irsa_arn
+    value = module.karpenter[0].irsa_arn
   }
 
   set {
