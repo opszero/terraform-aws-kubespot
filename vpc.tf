a9b821651bc00593f5c791555905caca1a80b5ab
Abhi Yerra <abhi@berkeley.edu>
2023-02-01 20:39:23 -0800

    Added eks cluster

--- vpc.tf
+++ vpc.tf
@@ -112,6 +112,6 @@ resource "aws_security_group_rule" "node-ingress-karpenter" {
   to_port                  = 8443
   protocol                 = "-1"
   security_group_id        = aws_security_group.node.id
-  source_security_group_id = aws_security_group.node.id
+  source_security_group_id = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
   type                     = "ingress"
 }
