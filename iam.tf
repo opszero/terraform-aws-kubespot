c1fe7f5a3da71a935ff51e66a28cadd04a6c0ea1
Abhi Yerra <abhi@berkeley.edu>
2023-02-01 20:26:47 -0800

    Fix

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
-  url             = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+  thumbprint_list = [data.tls_certificate.cluster.certificates[*].sha1_fingerprint]
+  url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
   tags            = local.tags
 }
 
