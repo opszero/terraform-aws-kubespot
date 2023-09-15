d6933f0d0a443578778776618ae9cf0ecff54218
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2023-09-16 02:57:27 +0630

    Update node.tf

--- node.tf
+++ node.tf
@@ -10,7 +10,6 @@ set -o xtrace
 
 /etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority[0].data}' '${var.environment_name}'
 USERDATA
-
 }
 
 resource "aws_launch_configuration" "asg_nodes" {
