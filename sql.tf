99d90430edbf71a1a444c89256f76b752f652544
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2023-09-19 22:51:21 +0630

    Update sql.tf

--- sql.tf
+++ sql.tf
@@ -5,6 +5,10 @@ resource "aws_db_subnet_group" "default" {
   tags = local.tags
 }
 
+resource "aws_kms_key" "example" {
+  description = "Example KMS Key"
+}
+
 resource "aws_rds_cluster" "default" {
   count = var.sql_cluster_enabled ? 1 : 0
 
@@ -13,9 +17,10 @@ resource "aws_rds_cluster" "default" {
   engine_mode        = var.sql_engine_mode
   engine_version     = var.sql_engine_version
 
-  database_name   = var.sql_database_name
-  master_username = var.sql_master_username
-  master_password = var.sql_master_password
+  database_name                 = var.sql_database_name
+  master_username               = var.sql_master_username
+  manage_master_user_password   = true
+  master_user_secret_kms_key_id = aws_kms_key.example.key_id
 
   db_subnet_group_name            = aws_db_subnet_group.default.name
   vpc_security_group_ids          = [aws_security_group.node.id]
@@ -28,8 +33,6 @@ resource "aws_rds_cluster" "default" {
   backup_retention_period   = 20
   skip_final_snapshot       = var.sql_skip_final_snapshot
   final_snapshot_identifier = var.environment_name
-
-  iam_database_authentication_enabled = var.sql_iam_auth_enabled
 
   tags = local.tags
 }
@@ -44,7 +47,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
 
   monitoring_role_arn          = var.monitoring_role_arn
   monitoring_interval          = 5
-  performance_insights_enabled = var.sql_performance_insights_enabled
+  performance_insights_enabled = var.performance_insights_enabled
 
   db_subnet_group_name    = aws_db_subnet_group.default.name
   db_parameter_group_name = var.sql_parameter_group_name == "" ? null : var.sql_parameter_group_name
@@ -55,19 +58,19 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
 resource "aws_db_instance" "default" {
   count = var.sql_instance_enabled ? 1 : 0
 
-  identifier                   = var.sql_identifier != "" ? var.sql_identifier : var.environment_name
-  allocated_storage            = var.sql_instance_allocated_storage
-  max_allocated_storage        = var.sql_instance_max_allocated_storage
-  performance_insights_enabled = var.sql_performance_insights_enabled
+  identifier            = var.sql_identifier != "" ? var.sql_identifier : var.environment_name
+  allocated_storage     = var.sql_instance_allocated_storage
+  max_allocated_storage = var.sql_instance_max_allocated_storage
 
-  storage_type   = var.sql_storage_type
-  engine         = var.sql_instance_engine
-  engine_version = var.sql_engine_version
-  instance_class = var.sql_instance_class
-  db_name        = var.sql_database_name
-  username       = var.sql_master_username
-  password       = var.sql_master_password
-  multi_az       = var.sql_rds_multi_az
+  storage_type                  = var.sql_storage_type
+  engine                        = var.sql_instance_engine
+  engine_version                = var.sql_engine_version
+  instance_class                = var.sql_instance_class
+  db_name                       = var.sql_database_name
+  username                      = var.sql_master_username
+  manage_master_user_password   = true
+  master_user_secret_kms_key_id = aws_kms_key.example.key_id
+  multi_az                      = var.sql_rds_multi_az
 
   db_subnet_group_name   = aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.node.id]
@@ -77,8 +80,6 @@ resource "aws_db_instance" "default" {
   allow_major_version_upgrade = true
   backup_retention_period     = 35
   deletion_protection         = true // Don't Delete Ever! Except manually.
-
-  iam_database_authentication_enabled = var.sql_iam_auth_enabled
 
   tags = local.tags
 }
