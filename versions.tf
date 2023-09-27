ae7e9e3fdc48ec9632d69e75259e6eaea644475c
Abhi Yerra <abhi@berkeley.edu>
2023-09-27 10:16:08 -0700

    Address version incompatability

--- versions.tf
+++ versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.17.0"
+      version = ">= 5.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = ">= 2.23"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.10.1"
+      version = ">= 2.11"
     }
   }
 }
