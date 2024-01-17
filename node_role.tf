e7e8b5b2447003e0fb690af18148a3694b2e9b80
Jana <jana@opszero.com>
2024-01-17 22:25:08 +0100

    Fix eks pod logs to cloudwatch

--- node_role.tf
+++ node_role.tf
@@ -38,8 +38,8 @@ resource "aws_iam_role_policy_attachment" "node_role_policies" {
 
 
 resource "aws_iam_policy" "eks_pod_logs_to_cloudwatch" {
-  count       = var.eks_pod_logs_cloudwatch ? 1 : 0
-  name        = "nodeEksPodLogsToCloudwatch"
+  count       = var.enable_pods_logs_to_cloudwatch ? 1 : 0
+  name        = "${var.environment_name}-EksPodLogsToCloudwatch"
   description = "Used by fluentbit agent to send eks pods logs to cloudwatch"
 
   policy = <<EOF
@@ -55,7 +55,7 @@ resource "aws_iam_policy" "eks_pod_logs_to_cloudwatch" {
           "logs:CreateLogGroup",
           "logs:PutLogEvents"
       ],
-      "Resource": [*]
+      "Resource": "*"
     }
   ]
 }
@@ -64,8 +64,8 @@ EOF
 
 
 resource "aws_iam_role_policy_attachment" "node_eks_pod_logs_to_cloudwatch" {
-  count      = var.eks_pod_logs_cloudwatch ? 1 : 0
-  policy_arn = aws_iam_policy.eks_pod_logs_to_cloudwatch.arn
+  count      = var.enable_pods_logs_to_cloudwatch ? 1 : 0
+  policy_arn = aws_iam_policy.eks_pod_logs_to_cloudwatch[count.index].arn
   role       = aws_iam_role.node.name
 }
 
