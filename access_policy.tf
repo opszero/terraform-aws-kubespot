da7d5891c4e8299e39d1e3ff59b31d9f1a95f7d7
Abhi Yerra <abhi@berkeley.edu>
2024-01-23 19:10:46 -0800

    Fix

--- access_policy.tf
+++ access_policy.tf
@@ -3,5 +3,11 @@ resource "aws_eks_access_policy_association" "policies" {
   cluster_name  = aws_eks_cluster.cluster.name
   principal_arn = lookup(var.access_policies[count.index], "principal_arn")
   policy_arn    = lookup(var.access_policies[count.index], "policy_arn")
-  access_scope  = lookup(var.access_policies[count.index], "access_scope", { type = "cluster" })
+  dynamic "access_scope" {
+    for_each = var.access_policies[count.index].access_scope
+    content {
+      type       = lookup(access_scope, "type", "cluster")
+      namespaces = lookup(access_scope, "namespaces", ["default"])
+    }
+  }
 }
