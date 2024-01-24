d4f43525ccb71c0efdcc8356d640e12ab0fe8b5c
Abhi Yerra <abhi@berkeley.edu>
2024-01-23 18:38:58 -0800

    Disable

--- cluster.tf
+++ cluster.tf
@@ -26,8 +26,7 @@ resource "aws_eks_cluster" "cluster" {
   }
 
   access_config {
-    authentication_mode                         = "API_AND_CONFIG_MAP"
-    bootstrap_cluster_creator_admin_permissions = true
+    authentication_mode = "API_AND_CONFIG_MAP"
   }
 
   dynamic "encryption_config" {
