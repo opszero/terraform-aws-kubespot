ea4952040ce28d5aad572374d0a5a273d00805e4
Abhi Yerra <abhi@berkeley.edu>
2024-02-05 11:12:37 -0800

    Added depends

--- karpenter.tf
+++ karpenter.tf
@@ -25,6 +25,10 @@ module "karpenter" {
   queue_name              = "${var.environment_name}-spot-termination"
 
   tags = local.tags
+
+  depends_on = [
+    aws_eks_cluster.cluster
+  ]
 }
 
 resource "helm_release" "karpenter" {
