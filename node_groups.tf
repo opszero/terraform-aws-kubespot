6c0f13f8c2f4a9e0ee4e2de41ed2fc56f9ca3c03
Abhi Yerra <abhi@berkeley.edu>
2024-03-13 15:55:22 -0700

    Added cluster service cidr

--- node_groups.tf
+++ node_groups.tf
@@ -7,9 +7,10 @@ module "eks_mng_bottlerocket_custom_ami" {
 
   platform = "bottlerocket"
 
-  cluster_name        = var.environment_name
-  cluster_endpoint    = aws_eks_cluster.cluster.endpoint
-  cluster_auth_base64 = aws_eks_cluster.cluster.certificate_authority[0].data
+  cluster_name         = var.environment_name
+  cluster_endpoint     = aws_eks_cluster.cluster.endpoint
+  cluster_auth_base64  = aws_eks_cluster.cluster.certificate_authority[0].data
+  cluster_service_cidr = aws_eks_cluster.cluster.kubernetes_network_config.0.service_ipv4_cidr
 
   enable_bootstrap_user_data = true
 
