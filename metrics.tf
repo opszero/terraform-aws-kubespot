c9736c90db4fda46e64c001122d6aa85afcaed4b
Abhi Yerra <abhi@berkeley.edu>
2024-03-14 22:59:21 -0700

    UserData for Custom Node Groups and SOC2 Metrics (#371)
    * Fix
    
    * Added node group
    
    * Added arn
    
    * Fix name
    
    * Added IO monitoring
    
    * Added rds metrics
    
    * Added cluster service cidr
    
    * Added
    
    * Fix version
    
    * Test tfsec

--- metrics.tf
+++ metrics.tf
@@ -50,6 +50,27 @@ resource "aws_cloudwatch_metric_alarm" "database_cpu_database" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "database_cpu_database-rds" {
+  count                     = var.sql_instance_enabled ? 1 : 0
+  alarm_name                = "${var.environment_name}-cpu-database"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/RDS"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors RDS cpu utilization"
+  insufficient_data_actions = []
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.default[0].identifier
+  }
+  tags = {
+    "KubespotEnvironment" = var.environment_name
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "database_disk_database" {
   count                     = var.sql_cluster_enabled ? 1 : 0
   alarm_name                = "${var.environment_name}-disk-database"
@@ -134,6 +155,8 @@ resource "aws_cloudwatch_metric_alarm" "database_free_disk_database3" {
   }
 }
 
+
+
 resource "aws_cloudwatch_metric_alarm" "database_free_disk_database4" {
   count                     = var.sql_cluster_enabled ? 1 : 0
   alarm_name                = "${var.environment_name}-free-disk-database4"
@@ -155,6 +178,45 @@ resource "aws_cloudwatch_metric_alarm" "database_free_disk_database4" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "database_free_disk_database5" {
+  count                     = var.sql_instance_enabled ? 1 : 0
+  alarm_name                = "${var.environment_name}-free-disk-database5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "FreeLocalStorage"
+  namespace                 = "AWS/RDS"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors RDS free disk space"
+  insufficient_data_actions = []
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.default[0].identifier
+  }
+  tags = {
+    "KubespotEnvironment" = var.environment_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "database_io_rds" {
+  count                     = var.sql_instance_enabled ? 1 : 0
+  alarm_name                = "${var.environment_name}-io-postgres"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "DiskQueueDepth"
+  namespace                 = "AWS/RDS"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors RDS free disk space"
+  insufficient_data_actions = []
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.default[0].identifier
+  }
+  tags = local.tags
+}
 
 resource "aws_cloudwatch_metric_alarm" "database_io_postgres" {
   count                     = var.sql_cluster_enabled ? 1 : 0
@@ -193,3 +255,4 @@ resource "aws_cloudwatch_metric_alarm" "database_io_mysql" {
   }
   tags = local.tags
 }
+
