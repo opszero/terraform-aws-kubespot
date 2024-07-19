ee17d7785052ec9aa7157c31e06ac92091fe581d
Sohan Yadav <sohanyadavdevops@gmail.com>
2024-07-19 18:19:59 +0530

    enable csi

--- examples/eks/main.tf
+++ examples/eks/main.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-  # TODO: Change this
-  profile = "opszero"
-  # TODO: Change this
-  region = "us-west-2"
-}
+# provider "aws" {
+#   # TODO: Change this
+#   profile = "opszero"
+#   # TODO: Change this
+#   region = "us-west-2"
+# }
 
 locals {
   environment_name = "appcensus-dev"
-  profile          = "appcensus-staging"
+  profile          = "celebgaze-mew"
 }
 
 provider "aws" {
@@ -15,20 +15,20 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "./kubeconfig"
-  }
-}
-
-provider "kubernetes" {
-  config_path = "./kubeconfig"
-}
-
-
+# provider "helm" {
+#   kubernetes {
+#     config_path = "./kubeconfig"
+#   }
+# }
+#
+# provider "kubernetes" {
+#   config_path = "./kubeconfig"
+# }
+#
+#
 
 module "opszero-eks" {
-  source = "github.com/opszero/terraform-aws-kubespot"
+  source = "./../../"
 
   zones = [
     "us-east-1a",
@@ -37,19 +37,8 @@ module "opszero-eks" {
 
   cluster_version  = "1.27"
   environment_name = local.environment_name
-  iam_users = {
-    "abhi@opszero.com" = {
-      rbac_groups = [
-        "system:masters"
-      ]
-    },
-    "bitbucket-deployer" = {
-      rbac_groups = [
-        "system:masters"
-      ]
-    },
 
-  }
+
   cidr_block = "10.3.0.0/16"
   cidr_block_public_subnet = [
     "10.3.0.0/18",
@@ -59,31 +48,40 @@ module "opszero-eks" {
     "10.3.128.0/18",
     "10.3.192.0/18",
   ]
-
-  node_groups = {
-    "t3a-medium-spot" = {
-      # Have to use a custom launch template to get encrypted root volumes.
-      instance_types = [
-        "t3a.medium",
-      ]
-      capacity_type          = "SPOT"
-      nodes_in_public_subnet = false
-      node_desired_capacity  = 3,
-      nodes_max_size         = 3,
-      nodes_min_size         = 3
-      ami_type               = "CUSTOM"
-    },
-    "t3a-medium-spot2" = {
-      instance_types = [
-        "t3a.medium",
-      ]
-      node_disk_size         = 32
-      nodes_in_public_subnet = false
-      node_desired_capacity  = 1,
-      nodes_max_size         = 1,
-      nodes_min_size         = 1
-    }
+asg_nodes = {
+  "test" = {
+    nodes_desired_capacity  = 2
+    nodes_max_size          = 5
+    nodes_min_size          = 1
+    node_disk_encrypted     = true
+    node_disk_size          = 32
+    nodes_in_public_subnet  = false
   }
+}
+#   node_groups = {
+#     "t3a-medium-spot" = {
+#       # Have to use a custom launch template to get encrypted root volumes.
+#       instance_types = [
+#         "t3a.medium",
+#       ]
+#       capacity_type          = "SPOT"
+#       nodes_in_public_subnet = false
+#       node_desired_capacity  = 3,
+#       nodes_max_size         = 3,
+#       nodes_min_size         = 3
+#       ami_type               = "CUSTOM"
+#     },
+#     "t3a-medium-spot2" = {
+#       instance_types = [
+#         "t3a.medium",
+#       ]
+#       node_disk_size         = 32
+#       nodes_in_public_subnet = false
+#       node_desired_capacity  = 1,
+#       nodes_max_size         = 1,
+#       nodes_min_size         = 1
+#     }
+#   }
 
   redis_enabled        = false
   sql_cluster_enabled  = false
@@ -93,15 +91,15 @@ module "opszero-eks" {
   vpc_flow_logs_enabled = false
   efs_enabled           = false
   # csi
-  s3_csi_driver_enabled = true
-  csi_bucket_name       = "test-6647373dd" #name of s3
+#   s3_csi_driver_enabled = false
+#   csi_bucket_name       = "test-6647373dd" #name of s3
 }
 
-module "helm-common" {
-  source             = "github.com/opszero/terraform-helm-kubespot"
-  cert_manager_email = "ops@opszero.com"
-
-  nginx_min_replicas = 1
-  nginx_max_replicas = 3
-}
+# module "helm-common" {
+#   source             = "github.com/opszero/terraform-helm-kubespot"
+#   cert_manager_email = "ops@opszero.com"
+#
+#   nginx_min_replicas = 1
+#   nginx_max_replicas = 3
+# }
 
