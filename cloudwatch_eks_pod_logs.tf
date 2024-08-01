bed238bf6d030f054dfb45f88d4cc71d525f3a52
Abhi Yerra <abhi@berkeley.edu>
2024-08-01 11:43:07 -0700

    Update cloudwatch_eks_pod_logs.tf

--- cloudwatch_eks_pod_logs.tf
+++ cloudwatch_eks_pod_logs.tf
@@ -23,25 +23,3 @@ resource "kubernetes_config_map" "fluent_bit_cluster_info" {
     "logs.region"  = data.aws_region.current.name
   }
 }
-
-data "http" "fluent_bit_yaml" {
-  url = "https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/k8s/${local.eks_pod_logs_cloudwatch_fluent_bit_version}/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/fluent-bit/fluent-bit.yaml"
-}
-
-
-resource "null_resource" "eks_pod_cloudwatch" {
-  count = var.cloudwatch_pod_logs_enabled ? 1 : 0
-
-  triggers = {
-    manifest_sha1 = sha1(data.http.fluent_bit_yaml.body)
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl replace -f https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/k8s/${local.eks_pod_logs_cloudwatch_fluent_bit_version}/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/fluent-bit/fluent-bit.yaml"
-  }
-
-  depends_on = [
-    kubernetes_namespace.amazon_cloudwatch,
-    kubernetes_config_map.fluent_bit_cluster_info
-  ]
-}
