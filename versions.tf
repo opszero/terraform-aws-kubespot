b2e11ae3065cf2bfb064eb4cc8bbde5a59ecc545
Abhi Yerra <abhi@berkeley.edu>
2024-10-10 14:19:08 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,2 @@
 
-terraform {
-  required_version = ">= 1.6.6"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.31.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.23"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.11"
-    }
-  }
-}
+
