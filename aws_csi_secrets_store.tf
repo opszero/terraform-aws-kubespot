34ae8ec6c35e88f626efa2e402170436336a3257
Sohan Yadav <31764946+sohanyadav@users.noreply.github.com>
2024-11-07 22:34:13 +0530

    Fix secrets (#432)
    * fix sercrets
    
    * run fmt

--- aws_csi_secrets_store.tf
+++ aws_csi_secrets_store.tf
@@ -42,7 +42,7 @@ resource "null_resource" "csi_secrets_store_aws_provider" {
 
 
 resource "aws_iam_policy" "secrets_policy" {
-  count = var.csi_secrets_store_enabled ? 1 : 0
+  count       = var.csi_secrets_store_enabled ? 1 : 0
   name        = "csi-secrets-access-policy-${var.environment_name}"
   description = "Policy for accessing secrets in AWS Secrets Manager"
 
@@ -95,25 +95,26 @@ data "aws_iam_policy_document" "trust_relationship" {
 }
 
 resource "aws_iam_role" "secrets_manager_role" {
-  count = var.csi_secrets_store_enabled ? 1 : 0
+  count              = var.csi_secrets_store_enabled ? 1 : 0
   name               = "shared_secrets_manager_role"
   assume_role_policy = data.aws_iam_policy_document.trust_relationship.json
 }
 
 resource "aws_iam_role_policy_attachment" "secrets_manager_attachment" {
-  role       = join("",aws_iam_role.secrets_manager_role.*.name)
-  policy_arn = join ("", aws_iam_policy.secrets_policy.*.arn)
+  count      = var.csi_secrets_store_enabled ? 1 : 0
+  role       = join("", aws_iam_role.secrets_manager_role.*.name)
+  policy_arn = join("", aws_iam_policy.secrets_policy.*.arn)
 }
 
 
 resource "kubernetes_service_account" "main" {
-  for_each = toset(var.csi_enabled_namespaces) 
+  for_each = toset(var.csi_enabled_namespaces)
 
   metadata {
     name      = "csi-secrets-service-account"
     namespace = each.key
     annotations = {
-      "eks.amazonaws.com/role-arn" = join("",aws_iam_role.secrets_manager_role.*.arn)
+      "eks.amazonaws.com/role-arn" = join("", aws_iam_role.secrets_manager_role.*.arn)
     }
   }
 }
